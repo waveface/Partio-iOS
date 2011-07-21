7a0d2e7f14f386914870757f5e13246ea92d89da
Evadne Wu <ev@monoceroi.com>
2011-07-21 17:32:08 +0800

    header methods for WARemoteInterface

---
+++ wammer-iOS/WARemoteInterface.m
@@ -0,0 +1,23 @@
+//
+//  WARemoteInterface.m
+//  wammer-iOS
+//
+//  Created by Evadne Wu on 7/21/11.
+//  Copyright 2011 Iridia Productions. All rights reserved.
+//
+
+#import "WARemoteInterface.h"
+
+@implementation WARemoteInterface
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
