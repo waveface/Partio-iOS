5f54a6f64f7dd8a4be025175d27ba5e2220c2c7f
jsa :~) <james.sa.2013@gmail.com>
2011-07-29 16:16:50 +0800

    separate iPhone View

---
+++ wammer-iOS/WAArticlesViewController_iPhone.h
@@ -0,0 +1,15 @@
+//
+//  WAArticlesViewController.h
+//  wammer-iOS
+//
+//  Created by Evadne Wu on 7/20/11.
+//  Copyright 2011 Iridia Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface WAArticlesViewController_iPhone : UIViewController
+
+@end
