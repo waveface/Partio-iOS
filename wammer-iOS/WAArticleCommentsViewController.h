f4b2d569214187c33547e05230e384d49bdff9b8
Evadne Wu <ev@monoceroi.com>
2011-08-02 16:52:32 +0800

    removes an unnecessary WAPaginationSlider delegate method

---
+++ wammer-iOS/WAArticleCommentsViewController.h
@@ -0,0 +1,13 @@
+//
+//  WAArticleCommentsViewController.h
+//  wammer-iOS
+//
+//  Created by Evadne Wu on 8/2/11.
+//  Copyright 2011 Iridia Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WAArticleCommentsViewController : UIViewController
+
+@end
