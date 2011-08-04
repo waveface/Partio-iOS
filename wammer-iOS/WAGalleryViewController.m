4e2dffcb545d88163315747fddba7445f4d47a4f
Evadne Wu <ev@monoceroi.com>
2011-08-04 17:22:28 +0800

    initial gallery view scaffold.  updates Core Data model with new fetch request template.

--- wammer-iOS/WAGalleryViewController.m
+++ wammer-iOS/WAGalleryViewController.m
@@ -8,21 +8,52 @@
 
 #import "WAGalleryViewController.h"
 #import "IRPaginatedView.h"
+#import "WADataStore.h"
 
 
 @interface WAGalleryViewController () <IRPaginatedViewDelegate>
+@property (nonatomic, readwrite, retain) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, readwrite, retain) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic, readwrite, retain) WAArticle *article;
 @property (nonatomic, readwrite, retain) IRPaginatedView *paginatedView;
 @end
 
 
 @implementation WAGalleryViewController
+@synthesize managedObjectContext, fetchedResultsController, article;
 @synthesize paginatedView;
 
 + (WAGalleryViewController *) controllerRepresentingArticleAtURI:(NSURL *)anArticleURI {
 
 	WAGalleryViewController *returnedController = [[[self alloc] init] autorelease];
 	
+	returnedController.managedObjectContext = [[WADataStore defaultStore] disposableMOC];
+	returnedController.article = (WAArticle *)[returnedController.managedObjectContext irManagedObjectForURI:anArticleURI];
+	
 	return returnedController;
+
+}
+
+- (NSFetchedResultsController *) fetchedResultsController {
+
+	if (fetchedResultsController)
+		return fetchedResultsController;
+		
+	NSFetchRequest *fetchRequest = [self.managedObjectContext.persistentStoreCoordinator.managedObjectModel fetchRequestFromTemplateWithName:@"WAFRFilesForArticle" substitutionVariables:[NSDictionary dictionaryWithObjectsAndKeys:
+		self.article, @"Article",
+	nil]];
+	
+	fetchRequest.sortDescriptors = [NSArray arrayWithObjects:
+		[NSSortDescriptor sortDescriptorWithKey:@"timestamp" ascending:YES],
+	nil];
+		
+	self.fetchedResultsController = [[[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:self.managedObjectContext sectionNameKeyPath:nil cacheName:nil] autorelease];
+	
+	NSError *fetchingError;
+	if (![self.fetchedResultsController performFetch:&fetchingError])
+		NSLog(@"Error fetching: %@", fetchingError);
+	
+	return fetchedResultsController;
 
 }
 
@@ -52,14 +83,21 @@
 
 - (NSUInteger) numberOfViewsInPaginatedView:(IRPaginatedView *)paginatedView {
 
-	return 200;
+	return [self.article.files count];
 
 }
 
 - (UIView *) viewForPaginatedView:(IRPaginatedView *)aPaginatedView atIndex:(NSUInteger)index {
 
-	UIView *returnedView =  [[[UIView alloc] initWithFrame:aPaginatedView.bounds] autorelease];
-	returnedView.backgroundColor = [UIColor redColor];
+	WAFile *representedFile = (WAFile *)[self.fetchedResultsController.fetchedObjects objectAtIndex:index];
+	
+	NSString *resourceName = [NSString stringWithFormat:@"IPSample_%03i", (1 + (rand() % 48))];
+	NSString *resourceFilePath = [[[NSBundle mainBundle] URLForResource:resourceName withExtension:@"jpg" subdirectory:@"IPSample"] path];
+	
+	UIImageView *returnedView =  [[[UIImageView alloc] initWithFrame:aPaginatedView.bounds] autorelease];
+	returnedView.backgroundColor = [UIColor blackColor];
+	returnedView.image = [UIImage imageWithContentsOfFile:resourceFilePath];
+	returnedView.contentMode = UIViewContentModeScaleAspectFit;
 	
 	return returnedView;
 
@@ -74,5 +112,23 @@
 
 
 
+
+- (void) viewDidUnload {
+
+	self.paginatedView = nil;
+	
+	[super viewDidUnload];
+
+}
+
+- (void) dealloc {
+
+	[managedObjectContext release];
+	[article release];
+	[paginatedView release];
+	
+	[super dealloc];
+
+}
 
 @end
