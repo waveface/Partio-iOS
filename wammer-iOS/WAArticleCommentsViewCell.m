d21f3e90097328d0a63a60a0bfe6503447512266
Evadne Wu <ev@monoceroi.com>
2011-08-12 19:21:19 +0800

    initial swizzling implementation for WAArticleCommentsViewCell; defines various styles and a mechanism to load different XIBs for each style, too

---
+++ wammer-iOS/WAArticleCommentsViewCell.m
@@ -0,0 +1,97 @@
+//
+//  WAArticleCommentsViewCell.m
+//  wammer-iOS
+//
+//  Created by Evadne Wu on 8/12/11.
+//  Copyright 2011 Iridia Productions. All rights reserved.
+//
+
+#import "WAArticleCommentsViewCell.h"
+
+
+@interface WAArticleCommentsViewCell ()
+@property (nonatomic, readwrite, assign) WAArticleCommentsViewCellStyle style;
+@property (nonatomic, readwrite, copy) NSString *reuseIdentifier;
+@end
+
+
+@implementation WAArticleCommentsViewCell
+@synthesize style;
+@dynamic reuseIdentifier;
+@synthesize imageStackView, avatarView, userNicknameLabel, contentTextLabel, dateOriginLabel, dateLabel, originLabel;
+
+- (id) initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+
+	return [self initWithCommentsViewCellStyle:WAArticleCommentsViewCellStyleDefault reuseIdentifier:reuseIdentifier];
+
+}
+
+- (id) initWithCommentsViewCellStyle:(WAArticleCommentsViewCellStyle)aStyle reuseIdentifier:(NSString *)aReuseIdentifier {
+
+	NSString *loadedNibName = nil;
+	
+	switch (aStyle) {
+		case WAArticleCommentsViewCellStyleDefault: {
+			loadedNibName = @"WAArticleCommentsViewCell-Default";
+			break;
+		}
+		case WAArticleCommentsViewCellStyleImageStack: {
+			loadedNibName = @"WAArticleCommentsViewCell-ImageStack";
+			break;
+		}
+	}
+
+	self = [[[self class] cellFromNibNamed:loadedNibName instantiatingOwner:nil withOptions:nil] retain];
+	
+	if (!self)
+		return nil;
+	
+	self.style = aStyle;
+	self.reuseIdentifier = aReuseIdentifier;
+	
+	return self;
+	
+}
+
+- (void) dealloc {
+
+	[imageStackView release];
+	[avatarView release];
+	[userNicknameLabel release];
+	[contentTextLabel release];
+	[dateOriginLabel release];
+	[dateLabel release];
+	[originLabel release];
+	
+	[super dealloc];
+	
+}
+
+@end
+
+
+
+
+
+@implementation WAArticleCommentsViewCell (NibLoading)
+
++ (WAArticleCommentsViewCell *) cellFromNib {
+
+	return [self cellFromNibNamed:NSStringFromClass([self class]) instantiatingOwner:nil withOptions:nil];
+
+}
+
++ (WAArticleCommentsViewCell *) cellFromNibNamed:(NSString *)nibName instantiatingOwner:(id)owner withOptions:(NSDictionary *)options {
+
+	UINib *nib = [UINib nibWithNibName:nibName bundle:[NSBundle mainBundle]];
+	NSArray *loadedObjects = [nib instantiateWithOwner:nil options:nil];
+	
+	for (id loadedObject in loadedObjects)	
+	if ([loadedObject isKindOfClass:[self class]])
+		return loadedObject;
+	
+	return [[NSSet setWithArray:loadedObjects] anyObject];
+
+}
+
+@end
