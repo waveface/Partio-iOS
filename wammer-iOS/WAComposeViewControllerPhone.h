0c18e0b7a378b1c2610b4e02202a1ee13ad87bac
Jamie Sa <james.sa.2013@gmail.com>
2011-08-12 11:46:48 +0800

    refactor and create skeleton

---
+++ wammer-iOS/WAComposeViewControllerPhone.h
@@ -0,0 +1,13 @@
+//
+//  WAComposeViewControllerPhone.h
+//  wammer-iOS
+//
+//  Created by jamie on 8/11/11.
+//  Copyright 2011 Iridia Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WAComposeViewControllerPhone : UIViewController
+
+@end
