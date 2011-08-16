c7672ca2d5c9054ab218f599a58e2e90e47edb5c
Evadne Wu <ev@monoceroi.com>
2011-08-16 17:38:37 +0800

    adds WASpeechBubble

---
+++ wammer-iOS/WAArticleTextEmphasisLabel.h
@@ -0,0 +1,13 @@
+//
+//  WAArticleTextEmphasisView.h
+//  wammer-iOS
+//
+//  Created by Evadne Wu on 8/16/11.
+//  Copyright 2011 Iridia Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WAArticleTextEmphasisView : UIView
+
+@end
---
+++ wammer-iOS/WAArticleTextEmphasisLabel.m
@@ -0,0 +1,31 @@
+//
+//  WAArticleTextEmphasisView.m
+//  wammer-iOS
+//
+//  Created by Evadne Wu on 8/16/11.
+//  Copyright 2011 Iridia Productions. All rights reserved.
+//
+
+#import "WAArticleTextEmphasisView.h"
+
+@implementation WAArticleTextEmphasisView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
