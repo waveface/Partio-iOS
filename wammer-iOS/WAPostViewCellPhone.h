881eddbad9e7805a6b0b46f5e9fa136e570f0dcf
Jamie Sa <james.sa.2013@gmail.com>
2011-08-18 14:41:44 +0800

    add post view

---
+++ wammer-iOS/WAPostViewCellPhone.h
@@ -0,0 +1,41 @@
+//
+//  WAArticleCommentsViewCell.h
+//  wammer-iOS
+//
+//  Created by Evadne Wu on 8/12/11.
+//  Copyright 2011 Iridia Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "WAImageStackView.h"
+
+enum {
+	WAPostViewCellStyleDefault,
+	WAPostViewCellStyleImageStack
+}; typedef NSUInteger WAPostViewCellStyle;
+
+
+@interface WAPostViewCellPhone : UITableViewCell
+
+- (id) initWithCommentsViewCellStyle:(WAPostViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier;
+
+@property (nonatomic, readwrite, retain) IBOutlet WAImageStackView *imageStackView;
+@property (nonatomic, readwrite, retain) IBOutlet UIImageView *avatarView;
+@property (nonatomic, readwrite, retain) IBOutlet UILabel *userNicknameLabel;
+@property (nonatomic, readwrite, retain) IBOutlet UILabel *contentTextLabel;
+@property (nonatomic, readwrite, retain) IBOutlet UILabel *dateOriginLabel;
+@property (nonatomic, readwrite, retain) IBOutlet UILabel *dateLabel;
+@property (nonatomic, readwrite, retain) IBOutlet UILabel *originLabel;
+
+@end
+
+
+
+
+
+@interface WAPostViewCellPhone (NibLoading)
+
++ (WAPostViewCellPhone *) cellFromNib;
++ (WAPostViewCellPhone *) cellFromNibNamed:(NSString *)nibName instantiatingOwner:(id)owner withOptions:(NSDictionary *)options;
+
+@end
---
+++ wammer-iOS/WAPostViewCellPhone.m
@@ -0,0 +1,97 @@
+//
+//  WAArticleCommentsViewCell.m
+//  wammer-iOS
+//
+//  Created by Evadne Wu on 8/12/11.
+//  Copyright 2011 Iridia Productions. All rights reserved.
+//
+
+#import "WAPostViewCellPhone.h"
+
+
+@interface WAPostViewCellPhone ()
+@property (nonatomic, readwrite, assign) WAPostViewCellStyle style;
+@property (nonatomic, readwrite, copy) NSString *reuseIdentifier;
+@end
+
+
+@implementation WAPostViewCellPhone
+@synthesize style;
+@dynamic reuseIdentifier;
+@synthesize imageStackView, avatarView, userNicknameLabel, contentTextLabel, dateOriginLabel, dateLabel, originLabel;
+
+- (id) initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+
+	return [self initWithCommentsViewCellStyle:WAPostViewCellStyleDefault reuseIdentifier:reuseIdentifier];
+
+}
+
+- (id) initWithCommentsViewCellStyle:(WAPostViewCellStyle)aStyle reuseIdentifier:(NSString *)aReuseIdentifier {
+
+	NSString *loadedNibName = nil;
+	
+	switch (aStyle) {
+		case WAPostViewCellStyleDefault: {
+			loadedNibName = @"WAPostViewCell-Default";
+			break;
+		}
+		case WAPostViewCellStyleImageStack: {
+			loadedNibName = @"WAPostViewCell-ImageStack";
+			break;
+		}
+	}
+
+	self = [[[self class] cellFromNibNamed:loadedNibName instantiatingOwner:nil withOptions:nil] retain];
+	
+	if (!self)
+		return nil;
+	
+	self.style = aStyle;
+	self.reuseIdentifier = aReuseIdentifier;
+	
+	return self;
+	
+}
+
+- (void) dealloc {
+
+	[imageStackView release];
+	[avatarView release];
+	[userNicknameLabel release];
+	[contentTextLabel release];
+	[dateOriginLabel release];
+	[dateLabel release];
+	[originLabel release];
+	
+	[super dealloc];
+	
+}
+
+@end
+
+
+
+
+
+@implementation WAPostViewCellPhone (NibLoading)
+
++ (WAPostViewCellPhone *) cellFromNib {
+
+	return [self cellFromNibNamed:NSStringFromClass([self class]) instantiatingOwner:nil withOptions:nil];
+
+}
+
++ (WAPostViewCellPhone *) cellFromNibNamed:(NSString *)nibName instantiatingOwner:(id)owner withOptions:(NSDictionary *)options {
+
+	UINib *nib = [UINib nibWithNibName:nibName bundle:[NSBundle mainBundle]];
+	NSArray *loadedObjects = [nib instantiateWithOwner:nil options:nil];
+	
+	for (id loadedObject in loadedObjects)	
+	if ([loadedObject isKindOfClass:[self class]])
+		return loadedObject;
+	
+	return [[NSSet setWithArray:loadedObjects] anyObject];
+
+}
+
+@end
