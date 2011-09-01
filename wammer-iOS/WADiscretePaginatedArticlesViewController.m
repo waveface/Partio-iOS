5c147167db4df352a7daf141e950d7864f65bbb0
Evadne Wu <ev@monoceroi.com>
2011-09-01 20:23:51 +0800

    adds discrete paginated articles view controller XIB to the project; wires up IBOutlets; ditches -loadView; does good animation removal during autorotation; uses fine layout grid locating mechanism

--- wammer-iOS/WADiscretePaginatedArticlesViewController.m
+++ wammer-iOS/WADiscretePaginatedArticlesViewController.m
@@ -6,9 +6,9 @@
 //  Copyright 2011 Iridia Productions. All rights reserved.
 //
 
+#import <QuartzCore/QuartzCore.h>
 #import "WADiscretePaginatedArticlesViewController.h"
 #import "IRDiscreteLayoutManager.h"
-#import "IRPaginatedView.h"
 #import "WADataStore.h"
 
 #import "WAArticleViewController.h"
@@ -18,36 +18,54 @@ static NSString * const kWADiscreteArticlePageElements = @"kWADiscreteArticlePag
 
 static NSString * const kWADiscreteArticleViewControllerOnItem = @"kWADiscreteArticleViewControllerOnItem";
 
-@interface WADiscretePaginatedArticlesViewController () <IRDiscreteLayoutManagerDelegate, IRDiscreteLayoutManagerDataSource, IRPaginatedViewDelegate, WAArticleViewControllerPresenting>
+@interface WADiscretePaginatedArticlesViewController () <IRDiscreteLayoutManagerDelegate, IRDiscreteLayoutManagerDataSource, WAArticleViewControllerPresenting>
 
 @property (nonatomic, readwrite, retain) IRDiscreteLayoutManager *discreteLayoutManager;
 @property (nonatomic, readwrite, retain) IRDiscreteLayoutResult *discreteLayoutResult;
 @property (nonatomic, readwrite, retain) NSArray *layoutGrids;
-@property (nonatomic, readwrite, retain) IRPaginatedView *paginatedView;
 
 - (UIView *) representingViewForItem:(WAArticle *)anArticle;
 - (void) adjustPageViewAtIndex:(NSUInteger)anIndex;
+- (void) adjustPageViewAtIndex:(NSUInteger)anIndex withAdditionalAdjustments:(void(^)(UIView *aSubview))aBlock;
+
+- (void) adjustPageView:(UIView *)aPageView usingGridAtIndex:(NSUInteger)anIndex;
 
 @end
 
 @implementation WADiscretePaginatedArticlesViewController
-@synthesize discreteLayoutManager, discreteLayoutResult, layoutGrids, paginatedView;
+@synthesize paginationSlider, discreteLayoutManager, discreteLayoutResult, layoutGrids, paginatedView;
 
-- (void) loadView {
+- (id) init {
 
-	NSLog(@"TBD remember previous paginated view page");
+	return [self initWithNibName:NSStringFromClass([self class]) bundle:[NSBundle bundleForClass:[self class]]];
 
-	self.view = [[[UIView alloc] initWithFrame:[UIScreen mainScreen].applicationFrame] autorelease];
-	self.view.backgroundColor = [UIColor colorWithWhite:0.9f alpha:1.0f];
-	
-	self.paginatedView = [[[IRPaginatedView alloc] initWithFrame:self.view.bounds] autorelease];
-	self.paginatedView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
-	self.paginatedView.delegate = self;
-	[self.view addSubview:self.paginatedView];
-	
+}
+
+//- (void) loadView {
+//
+//	self.view = [[[UIView alloc] initWithFrame:[UIScreen mainScreen].applicationFrame] autorelease];
+//	self.view.backgroundColor = [UIColor colorWithWhite:0.9f alpha:1.0f];
+//	
+//	self.paginatedView = [[[IRPaginatedView alloc] initWithFrame:self.view.bounds] autorelease];
+//	self.paginatedView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+//	self.paginatedView.delegate = self;
+//	[self.view addSubview:self.paginatedView];
+//	
+//	if (self.discreteLayoutResult)
+//		[self.paginatedView reloadViews];
+//	
+//}
+
+- (void) viewDidLoad {
+
+	[super viewDidLoad];
+
 	if (self.discreteLayoutResult)
 		[self.paginatedView reloadViews];
 	
+	self.paginationSlider.numberOfPages = self.paginatedView.numberOfPages;
+	self.paginationSlider.currentPage = self.paginatedView.currentPage;
+
 }
 
 - (UIView *) representingViewForItem:(WAArticle *)anArticle {
@@ -67,6 +85,7 @@ static NSString * const kWADiscreteArticleViewControllerOnItem = @"kWADiscreteAr
 	articleViewController.view.clipsToBounds = YES;
 	articleViewController.view.layer.borderColor = [UIColor colorWithWhite:0.9f alpha:1.0f].CGColor;
 	articleViewController.view.layer.borderWidth = 1.0f;
+	((UIView *)articleViewController.imageStackView).userInteractionEnabled = NO;
 	
 	articleViewController.onPresentingViewController = ^ (void(^action)(UIViewController <WAArticleViewControllerPresenting> *parentViewController)) {
 	
@@ -124,7 +143,7 @@ static NSString * const kWADiscreteArticleViewControllerOnItem = @"kWADiscreteAr
 	
 	self.layoutGrids = [NSArray arrayWithObjects:
 		(UIInterfaceOrientationIsPortrait(self.interfaceOrientation) ? portraitGrid : landscapeGrid),
-	nil];
+	nil];	
 	
 	self.discreteLayoutManager = [[IRDiscreteLayoutManager new] autorelease];
 	self.discreteLayoutManager.delegate = self;
@@ -189,6 +208,7 @@ static NSString * const kWADiscreteArticleViewControllerOnItem = @"kWADiscreteAr
 - (UIView *) viewForPaginatedView:(IRPaginatedView *)aPaginatedView atIndex:(NSUInteger)index {
 
 	UIView *returnedView = [[[UIView alloc] initWithFrame:aPaginatedView.bounds] autorelease];
+	returnedView.autoresizingMask = UIViewAutoresizingNone;
 	
 	IRDiscreteLayoutGrid *viewGrid = (IRDiscreteLayoutGrid *)[self.discreteLayoutResult.grids objectAtIndex:index];
 	
@@ -204,9 +224,15 @@ static NSString * const kWADiscreteArticleViewControllerOnItem = @"kWADiscreteAr
 		[pageElements addObject:placedSubview];
 		[returnedView addSubview:placedSubview];
 		
+		NSLog(@"placedSubview %@", placedSubview);
+		
 	}];
 	
 	objc_setAssociatedObject(returnedView, &kWADiscreteArticlePageElements, pageElements, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+	
+	[returnedView setNeedsLayout];
+	
+	[self adjustPageView:returnedView usingGridAtIndex:index];
 			
 	return returnedView;
 
@@ -220,61 +246,98 @@ static NSString * const kWADiscreteArticleViewControllerOnItem = @"kWADiscreteAr
 
 - (void) adjustPageViewAtIndex:(NSUInteger)anIndex {
 
-	UIView *currentPageView = [self.paginatedView existingPageAtIndex:anIndex];	
-	NSArray *currentPageElements = objc_getAssociatedObject(currentPageView, &kWADiscreteArticlePageElements);
-	IRDiscreteLayoutGrid *currentPageGrid = [self.discreteLayoutResult.grids objectAtIndex:anIndex];
-	
-	NSSet *allDestinations = [currentPageGrid allTransformablePrototypeDestinations];
-	
+	[self adjustPageViewAtIndex:anIndex withAdditionalAdjustments:nil];
+
+}
+
+- (void) adjustPageView:(UIView *)currentPageView usingGridAtIndex:(NSUInteger)anIndex {
+
 	//	Find the best grid alternative in allDestinations, and then enumerate its layout areas, using the provided layout blocks to relayout all the element representing views in the current paginated view page.
 	
-	if ([allDestinations count] != 1) {
-		currentPageGrid.contentSize = self.view.bounds.size;
-		[currentPageGrid enumerateLayoutAreasWithBlock:^(NSString *name, id item, BOOL(^validatorBlock)(IRDiscreteLayoutGrid *self, id anItem), CGRect(^layoutBlock)(IRDiscreteLayoutGrid *self, id anItem), id(^displayBlock)(IRDiscreteLayoutGrid *self, id anItem)) {
-			((UIView *)[currentPageElements objectAtIndex:[currentPageGrid.layoutAreaNames indexOfObject:name]]).frame = layoutBlock(currentPageGrid, item);
-		}];
-		return;
+	NSArray *currentPageElements = objc_getAssociatedObject(currentPageView, &kWADiscreteArticlePageElements);
+	IRDiscreteLayoutGrid *currentPageGrid = [self.discreteLayoutResult.grids objectAtIndex:anIndex];
+	NSSet *allDestinations = [currentPageGrid allTransformablePrototypeDestinations];
+	NSSet *allIntrospectedGrids = [allDestinations setByAddingObject:currentPageGrid];
+	IRDiscreteLayoutGrid *bestGrid = nil;
+	CGFloat currentAspectRatio = CGRectGetWidth(self.paginatedView.frame) / CGRectGetHeight(self.paginatedView.frame);
+	for (IRDiscreteLayoutGrid *aGrid in allIntrospectedGrids) {
+		
+		CGFloat bestGridAspectRatio = bestGrid.contentSize.width / bestGrid.contentSize.height;
+		CGFloat currentGridAspectRatio = aGrid.contentSize.width / aGrid.contentSize.height;
+					
+		if (!bestGrid) {
+			bestGrid = [[aGrid retain] autorelease];
+			continue;
+		}
+		
+		if (fabs(currentAspectRatio - bestGridAspectRatio) < fabs(currentAspectRatio - currentGridAspectRatio)) {
+			continue;
+		}
+		
+		bestGrid = [[aGrid retain] autorelease];
+		
 	}
 	
-	
-	//	Assume that grid transforms are 1-to-1
-	
-	if ([allDestinations count] != 1)
-		return;
-		
-	IRDiscreteLayoutGrid *transformedGrid = [allDestinations anyObject];
+	IRDiscreteLayoutGrid *transformedGrid = bestGrid;//[allDestinations anyObject];
 	transformedGrid = [currentPageGrid transformedGridWithPrototype:(transformedGrid.prototype ? transformedGrid.prototype : transformedGrid)];
 	
-	transformedGrid.contentSize = self.view.bounds.size;
-	
+	CGSize oldContentSize = transformedGrid.contentSize;
+	transformedGrid.contentSize = self.paginatedView.frame.size;
 	[[currentPageGrid retain] autorelease];
-	
-	[[self.discreteLayoutResult mutableArrayValueForKey:@"grids"] replaceObjectAtIndex:anIndex withObject:transformedGrid];
-	
+			
 	[transformedGrid enumerateLayoutAreasWithBlock: ^ (NSString *name, id item, BOOL(^validatorBlock)(IRDiscreteLayoutGrid *self, id anItem), CGRect(^layoutBlock)(IRDiscreteLayoutGrid *self, id anItem), id(^displayBlock)(IRDiscreteLayoutGrid *self, id anItem)) {
 	
 		((UIView *)[currentPageElements objectAtIndex:[currentPageGrid.layoutAreaNames indexOfObject:name]]).frame = layoutBlock(transformedGrid, item);
 		
 	}];
+	
+	transformedGrid.contentSize = oldContentSize;
+
+}
+
+- (void) adjustPageViewAtIndex:(NSUInteger)anIndex withAdditionalAdjustments:(void(^)(UIView *aSubview))aBlock {
+
+	UIView *currentPageView = [self.paginatedView existingPageAtIndex:anIndex];	
+	[self adjustPageView:currentPageView usingGridAtIndex:anIndex];
+		
+	if (aBlock)
+		aBlock(currentPageView);
 
 }
 
 - (void) willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
 
 	[super willAnimateRotationToInterfaceOrientation:toInterfaceOrientation duration:duration];
+	
+	void (^removeAnimations)(UIView *) = ^ (UIView *introspectedView) {
+	
+		__block void (^removeAnimationsOnView)(UIView *aView) = nil;
+		
+		removeAnimationsOnView = ^ (UIView *aView) {
+		
+			[aView.layer removeAllAnimations];
 
+			for (UIView *aSubview in aView.subviews)
+				removeAnimationsOnView(aSubview);
+		
+		};
+		
+		removeAnimationsOnView(introspectedView);
+
+	};
+	
 	[CATransaction begin];
 	[CATransaction setDisableActions:YES];
 
 	//	If the paginated view is currently showing a view constructed with information provided by a layout grid, and that layout grid’s prototype has a fully transformable target, grab that transformable prototype and do a transform, then reposition individual items
 	
 	if (self.paginatedView.currentPage > 0)
-		[self adjustPageViewAtIndex:(self.paginatedView.currentPage - 1)];
+		[self adjustPageViewAtIndex:(self.paginatedView.currentPage - 1) withAdditionalAdjustments:removeAnimations];
 	
-	[self adjustPageViewAtIndex:self.paginatedView.currentPage];
+	[self adjustPageViewAtIndex:self.paginatedView.currentPage withAdditionalAdjustments:removeAnimations];
 	
 	if ((self.paginatedView.currentPage + 1) < self.paginatedView.numberOfPages) {
-		[self adjustPageViewAtIndex:(self.paginatedView.currentPage + 1)];
+		[self adjustPageViewAtIndex:(self.paginatedView.currentPage + 1) withAdditionalAdjustments:removeAnimations];
 	}
 	
 	[CATransaction commit];
