5507650f24c9f1cd9ddd2ed36014323431d44c0e
Evadne Wu <ev@monoceroi.com>
2011-09-06 18:03:54 +0800

    adds PLCrashReporter.

---
+++ wammer-iOS/UIApplication+CrashReporting.h
@@ -0,0 +1,13 @@
+//
+//  UIApplication+CrashReporting.h
+//  wammer-iOS
+//
+//  Created by Evadne Wu on 9/6/11.
+//  Copyright (c) 2011 Iridia Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIApplication_CrashReporting : NSObject
+
+@end
---
+++ wammer-iOS/UIApplication+CrashReporting.m
@@ -0,0 +1,13 @@
+//
+//  UIApplication+CrashReporting.m
+//  wammer-iOS
+//
+//  Created by Evadne Wu on 9/6/11.
+//  Copyright (c) 2011 Iridia Productions. All rights reserved.
+//
+
+#import "UIApplication+CrashReporting.h"
+
+@implementation UIApplication_CrashReporting
+
+@end
