7782abac636e290f7ca0722e2b0731a935a47bc4
Evadne Wu <ev@monoceroi.com>
2011-09-15 01:58:46 +0800

    initial work on OS X compatibility for IRWebAPIKit, IRFoundations
    ter the commit message for your changes. Lines starting

---
+++ wammer-OSX/WAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WAAppDelegate.h
+//  wammer-OSX
+//
+//  Created by Evadne Wu on 9/15/11.
+//  Copyright (c) 2011 Iridia Productions. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WAAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ wammer-OSX/WAAppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  WAAppDelegate.m
+//  wammer-OSX
+//
+//  Created by Evadne Wu on 9/15/11.
+//  Copyright (c) 2011 Iridia Productions. All rights reserved.
+//
+
+#import "WAAppDelegate.h"
+
+@implementation WAAppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
