983b62fa2123aa2ba4229aaca45e0bbb8de8a0c8
Evadne Wu <ev@monoceroi.com>
2011-09-15 01:59:04 +0800

    experimental wammer-OSX

--- wammer-OSX/WAAppDelegate.h
+++ wammer-OSX/WAAppDelegate.h
@@ -6,6 +6,7 @@
 //  Copyright (c) 2011 Iridia Productions. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
 #import <Cocoa/Cocoa.h>
 
 @interface WAAppDelegate : NSObject <NSApplicationDelegate>
--- wammer-OSX/WAAppDelegate.m
+++ wammer-OSX/WAAppDelegate.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2011 Iridia Productions. All rights reserved.
 //
 
+#import <UIKit/UIKitView.h>
 #import "WAAppDelegate.h"
 
 @implementation WAAppDelegate
@@ -20,6 +21,19 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
 	// Insert code here to initialize your application
+	
+	UIKitView *uiView = [[[UIKitView alloc] initWithFrame:[self.window.contentView bounds]] autorelease];
+	uiView.autoresizingMask = NSViewWidthSizable|NSViewHeightSizable;
+	[self.window.contentView addSubview:uiView];
+	[uiView.UIWindow addSubview:((^{
+		UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+		[button setTitle:@"Hello UIKit" forState:UIControlStateNormal];
+		[button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+		[button setContentEdgeInsets:(UIEdgeInsets){ 6, 8, 8, 8 }];
+		[button sizeToFit];
+		return button;
+	})())];
+	
 }
 
 @end
