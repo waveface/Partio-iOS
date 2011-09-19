3fbd2671d3899c948b20e20a21e0be2e15e80ed9
Evadne Wu <ev@monoceroi.com>
2011-09-19 15:45:38 +0800

    indentation

--- wammer/WARemoteInterface.m
+++ wammer/WARemoteInterface.m
@@ -586,131 +586,130 @@ static NSString *waErrorDomain = @"com.waveface.wammer.remoteInterface.error";
 
 - (void) uploadArticle:(NSURL *)anArticleURI onSuccess:(void (^)(void))successBlock onFailure:(void (^)(void))failureBlock {
 
-		if (!anArticleURI)
-			return;
+	NSParameterAssert(anArticleURI);
+
+	NSString *currentUserIdentifier = [[NSUserDefaults standardUserDefaults] objectForKey:@"WhoAmI"];
+	
+	NSManagedObjectContext *context = [[WADataStore defaultStore] disposableMOC];
+	WAArticle *updatedArticle = (WAArticle *)[context irManagedObjectForURI:anArticleURI];
 			
-		NSString *currentUserIdentifier = [[NSUserDefaults standardUserDefaults] objectForKey:@"WhoAmI"];
-		
+	void (^uploadArticleIfAppropriate)(NSURL *articleURL) = ^ (NSURL *articleURL) {
+	
 		NSManagedObjectContext *context = [[WADataStore defaultStore] disposableMOC];
-		WAArticle *updatedArticle = (WAArticle *)[context irManagedObjectForURI:anArticleURI];
-				
-		void (^uploadArticleIfAppropriate)(NSURL *articleURL) = ^ (NSURL *articleURL) {
+		WAArticle *updatedArticle = (WAArticle *)[context irManagedObjectForURI:articleURL];
+		
+		NSMutableArray *remoteFileIdentifiers = [NSMutableArray array];
+		
+		for (NSURL *aFileObjectURI in updatedArticle.fileOrder) {
+			WAFile *aFile = (WAFile *)[context irManagedObjectForURI:aFileObjectURI];
+			if (!aFile.identifier) {
+				NSLog(@"Article file %@ does not have a remote identifier; bailing upload pending future invocation.", aFile);
+				return;
+			}
+			[remoteFileIdentifiers addObject:aFile.identifier];
+		}
+		
+		[[WARemoteInterface sharedInterface] createArticleAsUser:currentUserIdentifier withText:updatedArticle.text attachments:remoteFileIdentifiers usingDevice:[UIDevice currentDevice].model onSuccess:^(NSDictionary *createdCommentRep) {
 		
 			NSManagedObjectContext *context = [[WADataStore defaultStore] disposableMOC];
-			WAArticle *updatedArticle = (WAArticle *)[context irManagedObjectForURI:articleURL];
+			context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy;
+			NSArray *touchedArticles = [WAArticle insertOrUpdateObjectsUsingContext:context withRemoteResponse:[NSArray arrayWithObject:createdCommentRep] usingMapping:[NSDictionary dictionaryWithObjectsAndKeys:
+	
+				@"WAFile", @"files",
+				@"WAComment", @"comments",
+				@"WAUser", @"owner",
+				@"WAPreview", @"previews",
 			
-			NSMutableArray *remoteFileIdentifiers = [NSMutableArray array];
+			nil] options:IRManagedObjectOptionIndividualOperations];
 			
-			for (NSURL *aFileObjectURI in updatedArticle.fileOrder) {
-				WAFile *aFile = (WAFile *)[context irManagedObjectForURI:aFileObjectURI];
-				if (!aFile.identifier) {
-					NSLog(@"Article file %@ does not have a remote identifier; bailing upload pending future invocation.", aFile);
-					return;
-				}
-				[remoteFileIdentifiers addObject:aFile.identifier];
+			for (WAArticle *anArticle in touchedArticles) {
+				anArticle.draft = (NSNumber *)kCFBooleanFalse;
+				NSLog(@"article %@, previews %@", anArticle, anArticle.previews);
 			}
 			
-			[[WARemoteInterface sharedInterface] createArticleAsUser:currentUserIdentifier withText:updatedArticle.text attachments:remoteFileIdentifiers usingDevice:[UIDevice currentDevice].model onSuccess:^(NSDictionary *createdCommentRep) {
+			if (successBlock)
+				successBlock();
 			
-				NSManagedObjectContext *context = [[WADataStore defaultStore] disposableMOC];
-				context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy;
-				NSArray *touchedArticles = [WAArticle insertOrUpdateObjectsUsingContext:context withRemoteResponse:[NSArray arrayWithObject:createdCommentRep] usingMapping:[NSDictionary dictionaryWithObjectsAndKeys:
+		} onFailure:^(NSError *error) {
 		
-					@"WAFile", @"files",
-					@"WAComment", @"comments",
-					@"WAUser", @"owner",
-					@"WAPreview", @"previews",
+			NSLog(@"Fail %@", error);
+			
+			if (failureBlock)
+				failureBlock();
+			
+		}];
+
+	};
+	
+	if (![updatedArticle.fileOrder count]) {
+	
+		//	If there are no attachments, all the merry
+		//	Just send the article out and call it done.
+		
+		uploadArticleIfAppropriate(anArticleURI);
+		return;
+		
+	}
+	
+	
+	//	Otherwise, work up a queue.
+	
+	dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+	dispatch_group_t group = dispatch_group_create();
+	
+	for (NSURL *aFileObjectURI in updatedArticle.fileOrder) {
+	
+		dispatch_group_async(group, queue, ^ {
+		
+			__block NSManagedObjectContext *context = [[WADataStore defaultStore] disposableMOC];
+			__block WAFile *updatedFile = (WAFile *)[context irManagedObjectForURI:aFileObjectURI];
+			
+			dispatch_queue_t currentQueue = dispatch_get_current_queue();
+			dispatch_retain(currentQueue);
+			
+			[[WARemoteInterface sharedInterface] uploadFileAtURL:[NSURL fileURLWithPath:updatedFile.resourceFilePath] asUser:currentUserIdentifier onSuccess:^(NSDictionary *uploadedFileRep) {
+			
+				//	Guarding against accidental crossing of thread boundaries
+				context = (id)0x1;
+				updatedFile = (id)0x1;
+			
+				NSManagedObjectContext *refreshingContext = [[WADataStore defaultStore] disposableMOC];
+				refreshingContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy;
 				
-				nil] options:IRManagedObjectOptionIndividualOperations];
+				WAFile *refreshedFile = (WAFile *)[refreshingContext irManagedObjectForURI:aFileObjectURI];
+				[refreshedFile configureWithRemoteDictionary:uploadedFileRep];
 				
-				for (WAArticle *anArticle in touchedArticles) {
-					anArticle.draft = (NSNumber *)kCFBooleanFalse;
-					NSLog(@"article %@, previews %@", anArticle, anArticle.previews);
-				}
+				NSError *savingError = nil;
+				if (![refreshingContext save:&savingError])
+					NSLog(@"Error saving: %@", savingError);
+					
+				NSURL *articleURL = [[refreshedFile.article objectID] URIRepresentation];
 				
-				if (successBlock)
-					successBlock();
+				dispatch_async(currentQueue, ^ {
+					uploadArticleIfAppropriate(articleURL);
+				});
+				
+				dispatch_release(currentQueue);
 				
 			} onFailure:^(NSError *error) {
 			
-				NSLog(@"Fail %@", error);
+				//	Guarding against accidental crossing of thread boundaries
+				context = (id)0x1;
+				updatedFile = (id)0x1;
 				
-				if (failureBlock)
-					failureBlock();
+				//	if (failureBlock)
+				//		failureBlock();
+				
+				NSLog(@"Failed uploading file: %@", error);
+				NSLog(@"TBD: handle this gracefully");
 				
 			}];
 
-		};
-		
-		if (![updatedArticle.fileOrder count]) {
-		
-			//	If there are no attachments, all the merry
-			//	Just send the article out and call it done.
-			
-			uploadArticleIfAppropriate(anArticleURI);
-			return;
-			
-		}
-		
-		
-		//	Otherwise, work up a queue.
-		
-		dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-		dispatch_group_t group = dispatch_group_create();
-		
-		for (NSURL *aFileObjectURI in updatedArticle.fileOrder) {
-		
-			dispatch_group_async(group, queue, ^ {
-			
-				__block NSManagedObjectContext *context = [[WADataStore defaultStore] disposableMOC];
-				__block WAFile *updatedFile = (WAFile *)[context irManagedObjectForURI:aFileObjectURI];
-				
-				dispatch_queue_t currentQueue = dispatch_get_current_queue();
-				dispatch_retain(currentQueue);
-				
-				[[WARemoteInterface sharedInterface] uploadFileAtURL:[NSURL fileURLWithPath:updatedFile.resourceFilePath] asUser:currentUserIdentifier onSuccess:^(NSDictionary *uploadedFileRep) {
-				
-					//	Guarding against accidental crossing of thread boundaries
-					context = (id)0x1;
-					updatedFile = (id)0x1;
-				
-					NSManagedObjectContext *refreshingContext = [[WADataStore defaultStore] disposableMOC];
-					refreshingContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy;
-					
-					WAFile *refreshedFile = (WAFile *)[refreshingContext irManagedObjectForURI:aFileObjectURI];
-					[refreshedFile configureWithRemoteDictionary:uploadedFileRep];
-					
-					NSError *savingError = nil;
-					if (![refreshingContext save:&savingError])
-						NSLog(@"Error saving: %@", savingError);
-						
-					NSURL *articleURL = [[refreshedFile.article objectID] URIRepresentation];
-					
-					dispatch_async(currentQueue, ^ {
-						uploadArticleIfAppropriate(articleURL);
-					});
-					
-					dispatch_release(currentQueue);
-					
-				} onFailure:^(NSError *error) {
-				
-					//	Guarding against accidental crossing of thread boundaries
-					context = (id)0x1;
-					updatedFile = (id)0x1;
-					
-					//	if (failureBlock)
-					//		failureBlock();
-					
-					NSLog(@"Failed uploading file: %@", error);
-					NSLog(@"TBD: handle this gracefully");
-					
-				}];
-
-			});
-		
-		}
-		
-		dispatch_release(group);
+		});
+	
+	}
+	
+	dispatch_release(group);
 
 }
 
