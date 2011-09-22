c7125b5beede8736cfc352cfb87d87239e4f9b15
Evadne Wu <ev@monoceroi.com>
2011-09-22 18:14:27 +0800

    adds WAArticle(DiscreteLayoutAdditions)

---
+++ wammer/WAArticle+DiscreteLayoutAdditions.m
@@ -0,0 +1,64 @@
+//
+//  WAArticle+DiscreteLayoutAdditions.m
+//  wammer
+//
+//  Created by Evadne Wu on 9/22/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import "WAArticle+DiscreteLayoutAdditions.h"
+#import "WAPreview.h"
+#import "WAFile.h"
+
+NSString * const WAArticle_DiscreteLayoutAdditions_ItemType = @"WAArticle_DiscreteLayoutAdditions_ItemType";
+
+@implementation WAArticle (DiscreteLayoutAdditions)
+
+- (NSString *) title {
+	
+	return self.title;
+	
+}
+
+- (NSArray *) representedMediaItems {
+
+	NSMutableArray *returnedArray = [NSMutableArray array];
+
+	for (WAFile *aFile in self.files) {
+		objc_setAssociatedObject(aFile, &WAArticle_DiscreteLayoutAdditions_ItemType, (id)kUTTypeImage, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+		[returnedArray addObject:aFile];
+	};
+	
+	for (WAPreview *aPreview in self.previews) {
+		objc_setAssociatedObject(aPreview, &WAArticle_DiscreteLayoutAdditions_ItemType, (id)kUTTypeURL, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+		[returnedArray addObject:aPreview];
+	};
+	
+	return returnedArray;
+	
+}
+
+- (CFStringRef) typeForRepresentedMediaItem:(id)anItem {
+	
+	CFStringRef returnedType = (CFStringRef)objc_getAssociatedObject(anItem, &WAArticle_DiscreteLayoutAdditions_ItemType);
+	
+	if (!returnedType)
+		returnedType = kUTTypeItem;
+	
+	return returnedType;
+	
+}
+
+- (NSString *) representedText {
+	return IRDiscreteLayoutItemContentMediaForUTIType(self, kUTTypeText);
+}
+
+- (NSURL *) representedImageURI {
+	return IRDiscreteLayoutItemContentMediaForUTIType(self, kUTTypeImage);
+}
+
+- (NSURL *) representedVideoURI {
+	return IRDiscreteLayoutItemContentMediaForUTIType(self, kUTTypeVideo);
+}
+
+@end
