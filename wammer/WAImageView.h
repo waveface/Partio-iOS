0bfe0819e7aa6c5d5efeaf9f8ed7540a796572ca
Evadne Wu <ev@monoceroi.com>
2011-10-02 20:01:54 +0800

    introduces WAImageView which does async image decoding

---
+++ wammer/WAImageView.h
@@ -0,0 +1,13 @@
+//
+//  WAImageView.h
+//  wammer
+//
+//  Created by Evadne Wu on 9/30/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WAImageView : UIImageView
+
+@end
