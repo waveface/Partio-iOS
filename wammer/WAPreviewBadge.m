f3f43f6543f483d5d3a1bfbad0e7ec64c26ebf31
Evadne Wu <ev@monoceroi.com>
2011-10-04 19:11:35 +0800

    WAPreviewBadge now uses custom typeface

--- wammer/WAPreviewBadge.m
+++ wammer/WAPreviewBadge.m
@@ -67,9 +67,10 @@ typedef enum {
 
 - (void) waSharedInit {
 
-	self.titleFont = [UIFont boldSystemFontOfSize:18.0f];
+//	self.titleFont = [UIFont boldSystemFontOfSize:18.0f];
+	self.titleFont = [UIFont fontWithName:@"Sansus Webissimo" size:32.0f];
 	self.titleColor = [UIColor colorWithRed:0 green:0 blue:0.45f alpha:1.0f];
-	self.textFont = [UIFont systemFontOfSize:16.0f];
+	self.textFont = [UIFont fontWithName:@"Palatino" size:16.0f];
 	self.textColor = [UIColor blackColor];
 	
 	self.backgroundColor = nil;
