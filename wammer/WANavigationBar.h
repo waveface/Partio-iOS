683e35e2dbda23fbf760ec542e1fa0ac7028c2f8
Evadne Wu <ev@monoceroi.com>
2011-10-04 21:00:24 +0800

    adds WANavigationBar

---
+++ wammer/WANavigationBar.h
@@ -0,0 +1,13 @@
+//
+//  WANavigationBar.h
+//  wammer
+//
+//  Created by Evadne Wu on 10/4/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WANavigationBar : UINavigationBar
+
+@end
---
+++ wammer/WANavigationBar.m
@@ -0,0 +1,19 @@
+//
+//  WANavigationBar.m
+//  wammer
+//
+//  Created by Evadne Wu on 10/4/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import "WANavigationBar.h"
+
+@implementation WANavigationBar
+
+- (void) drawRect:(CGRect)rect {
+
+		//	Nope.
+
+}
+
+@end
