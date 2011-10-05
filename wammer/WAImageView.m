eb6d82e1f5f9118c7c9b25df4ceb4f06d2c0b038
Evadne Wu <ev@monoceroi.com>
2011-10-05 16:37:21 +0800

    WAImageView is now based on a CATiledLayer

--- wammer/WAImageView.m
+++ wammer/WAImageView.m
@@ -6,21 +6,83 @@
 //  Copyright (c) 2011 Waveface. All rights reserved.
 //
 
-#import "WAImageView.h"
 #import <QuartzCore/QuartzCore.h> 
+#import <objc/runtime.h>
+
+#import "WAImageView.h"
 #import "UIImage+IRAdditions.h"
+
+
+static NSString * const kWAImageView_storedImage = @"kWAImageView_storedImage";
+
+
+@interface WAImageViewTiledLayer : CATiledLayer
+
+@end
+
+@implementation WAImageViewTiledLayer
+
++ (NSTimeInterval) fadeDuration {
+
+	return 0.0f;
+
+}
+
+@end
+
 
 @implementation WAImageView
 
++ (Class) layerClass {
+
+	return [WAImageViewTiledLayer class];
+
+}
+
+- (void) drawRect:(CGRect)rect {
+
+	//	NO OP
+
+}
+
+- (void) drawLayer:(CALayer *)layer inContext:(CGContextRef)ctx {
+
+	UIImage *ownImage = objc_getAssociatedObject(layer, &kWAImageView_storedImage);
+	if (!ownImage)
+		return;
+	
+	CGContextDrawImage(ctx, layer.frame, ownImage.CGImage);
+
+}
+
+- (void) setFrame:(CGRect)newFrame {
+
+	if (CGRectEqualToRect(newFrame, self.frame))
+		return;
+	
+	[super setFrame:newFrame];
+	
+	((CATiledLayer *)self.layer).tileSize = newFrame.size;
+	
+	self.layer.contents = nil;
+	[self.layer setNeedsDisplay];
+	
+}
+
 - (void) setImage:(UIImage *)newImage {
 
-	dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-		UIImage *decodedImage = [newImage irDecodedImage];
-		dispatch_async(dispatch_get_main_queue(), ^ {
-			[super setImage:decodedImage];
-		});
-	});
+	NSLog(@"%@, image %@ -> %@", self, self.image, newImage);
+
+	if (newImage == self.image)
+		return;
+		
+	[super setImage:newImage];
 	
+	self.layer.contents = nil;
+	[self.layer setNeedsDisplay];
+	
+	objc_setAssociatedObject(self.layer, &kWAImageView_storedImage, newImage, OBJC_ASSOCIATION_RETAIN);
+
 }
 
 @end
