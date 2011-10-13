3e93ad8fb23d4235f1bc58fcd1b6aa84611baacc
Evadne Wu <ev@monoceroi.com>
2011-10-14 01:38:17 +0800

    fixes unnecessary copy + autorelease in -[WADiscretePaginatedArticlesViewController reloadViewContents]

--- wammer/WADiscretePaginatedArticlesViewController.m
+++ wammer/WADiscretePaginatedArticlesViewController.m
@@ -441,9 +441,9 @@ static NSString * const kWADiscreteArticlesViewLastUsedLayoutGrids = @"kWADiscre
 - (void) reloadViewContents {
 
 	if (self.discreteLayoutResult) {
-		objc_setAssociatedObject(self, &kWADiscreteArticlesViewLastUsedLayoutGrids, [[[self.discreteLayoutResult.grids irMap: ^ (IRDiscreteLayoutGrid *aGridInstance, NSUInteger index, BOOL *stop) {
+		objc_setAssociatedObject(self, &kWADiscreteArticlesViewLastUsedLayoutGrids, [self.discreteLayoutResult.grids irMap: ^ (IRDiscreteLayoutGrid *aGridInstance, NSUInteger index, BOOL *stop) {
 			return [aGridInstance isFullyPopulated] ? aGridInstance.prototype : nil;
-		}] mutableCopy] autorelease], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+		}], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 	}
 	
 	self.discreteLayoutResult = [self.discreteLayoutManager calculatedResult];
