1d78d008c6a987763cf4f10bcbafef2ace613d80
Evadne Wu <ev@monoceroi.com>
2011-10-19 15:05:32 +0800

    adds more crash report recipients

--- wammer/WAAppDelegate.m
+++ wammer/WAAppDelegate.m
@@ -55,6 +55,9 @@
 	
 	[[UIApplication sharedApplication] setCrashReportRecipients:[NSArray arrayWithObjects:
 		@"Evadne Wu <evadne.wu@waveface.com>",
+		@"Vincent Huang <vincent.huang@waveface.com>",
+		@"Jamie Sa <jamie.sa@waveface.com",
+		@"Mineral <redmine@waveface.com",
 	nil]];
 	
 	self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
