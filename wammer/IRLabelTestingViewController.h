05fe898147e6ec604d57c036a19ff840dc348271
Evadne Wu <ev@monoceroi.com>
2011-10-19 14:56:03 +0800

    updates IRFoundations

---
+++ wammer/IRLabelTestingViewController.h
@@ -0,0 +1,13 @@
+//
+//  IRLabelTestingViewController.h
+//  wammer
+//
+//  Created by Evadne Wu on 10/19/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IRLabelTestingViewController : UIViewController
+
+@end
---
+++ wammer/IRLabelTestingViewController.m
@@ -0,0 +1,51 @@
+//
+//  IRLabelTestingViewController.m
+//  wammer
+//
+//  Created by Evadne Wu on 10/19/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import "IRLabelTestingViewController.h"
+
+@implementation IRLabelTestingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
