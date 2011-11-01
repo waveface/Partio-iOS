b6a71bdeb99f271c68eb81f4762198c5d9265af1
Evadne Wu <ev@monoceroi.com>
2011-11-01 20:04:32 +0800

    updates IRFounDations.
    f69ed664d0 (fixes bug where -[UIWindow irInterfaceBounds] may report an erroneous value)

---
+++ wammer/WACompositionViewController+CustomUI.h
@@ -0,0 +1,13 @@
+//
+//  WACompositionViewController+CustomUI.h
+//  wammer
+//
+//  Created by Evadne Wu on 11/1/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import "WACompositionViewController.h"
+
+@interface WACompositionViewController_CustomUI : WACompositionViewController
+
+@end
---
+++ wammer/WACompositionViewController+CustomUI.m
@@ -0,0 +1,13 @@
+//
+//  WACompositionViewController+CustomUI.m
+//  wammer
+//
+//  Created by Evadne Wu on 11/1/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import "WACompositionViewController+CustomUI.h"
+
+@implementation WACompositionViewController_CustomUI
+
+@end
