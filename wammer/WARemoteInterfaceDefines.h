9c0e9000fa72f21b4dfebb265fd2e38354061b80
Evadne Wu <ev@monoceroi.com>
2011-11-07 16:16:02 +0800

    updates IRWebAPIKit with form data concatenation

---
+++ wammer/WARemoteInterfaceDefines.h
@@ -0,0 +1,13 @@
+//
+//  WARemoteInterfaceDefines.h
+//  wammer
+//
+//  Created by Evadne Wu on 11/7/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol WARemoteInterfaceDefines <NSObject>
+
+@end
