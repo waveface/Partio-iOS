fd255cfb6539dde81ec94c2776bf12cbc64b8e39
Evadne Wu <ev@monoceroi.com>
2011-11-08 16:26:55 +0800

    adds WAGroup entity and moves the scheme to v.5

---
+++ wammer/WAGroup.h
@@ -0,0 +1,30 @@
+//
+//  WAGroup.h
+//  wammer
+//
+//  Created by Evadne Wu on 11/8/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+#import "CoreData+IRAdditions.h	"
+
+@class WAArticle;
+
+@interface WAGroup : IRManagedObject
+
+@property (nonatomic, retain) NSString * identifier;
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSString * text;
+@property (nonatomic, retain) NSSet *articles;
+@end
+
+@interface WAGroup (CoreDataGeneratedAccessors)
+
+- (void)addArticlesObject:(WAArticle *)value;
+- (void)removeArticlesObject:(WAArticle *)value;
+- (void)addArticles:(NSSet *)values;
+- (void)removeArticles:(NSSet *)values;
+
+@end
---
+++ wammer/WAGroup.m
@@ -0,0 +1,20 @@
+//
+//  WAGroup.m
+//  wammer
+//
+//  Created by Evadne Wu on 11/8/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import "WAGroup.h"
+#import "WAArticle.h"
+
+
+@implementation WAGroup
+
+@dynamic identifier;
+@dynamic title;
+@dynamic text;
+@dynamic articles;
+
+@end
