ce0a232bb465c5b29cdf7727e7efd06f77739207
Evadne Wu <ev@monoceroi.com>
2011-11-15 16:23:23 +0800

    adds a convenience method for adding comments on WADataStore’s remote interface additions

--- wammer/WADataStore+WARemoteInterfaceAdditions.m
+++ wammer/WADataStore+WARemoteInterfaceAdditions.m
@@ -88,4 +88,46 @@
 	
 }
 
+- (void) addComment:(NSString *)commentText onArticle:(NSURL *)anArticleURI onSuccess:(void(^)(void))successBlock onFailure:(void(^)(void))failureBlock {
+	
+	__block NSManagedObjectContext *context = [[self disposableMOC] retain];
+	__block WAArticle *updatedArticle = (WAArticle *)[context irManagedObjectForURI:anArticleURI];
+	
+	NSString *postIdentifier = updatedArticle.identifier;
+	NSString *groupIdentifier = updatedArticle.group.identifier;
+	
+	if (!postIdentifier) {
+		[NSException raise:NSInternalInconsistencyException format:@"Article %@ has not been saved, and was not assigned a remote identifier."];
+		return;
+	}
+	
+	if (!groupIdentifier) {
+		[NSException raise:NSInternalInconsistencyException format:@"Article %@ has not yet been assigned a group."];
+		return;
+	}
+	
+	[[WARemoteInterface sharedInterface] createCommentForPost:postIdentifier inGroup:groupIdentifier withContentText:commentText onSuccess:^(NSDictionary *updatedPostRep) {
+	
+		NSManagedObjectContext *context = [self disposableMOC];
+		context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy;
+		
+		NSArray *touchedPosts = [WAArticle insertOrUpdateObjectsUsingContext:context withRemoteResponse:[NSArray arrayWithObject:updatedPostRep] usingMapping:nil options:IRManagedObjectOptionIndividualOperations];
+		NSLog(@"%@", NSStringFromSelector(_cmd));
+		
+		NSError *savingError = nil;
+		if (![context save:&savingError])
+			NSLog(@"Error Saving: %@", savingError);
+		
+		if (successBlock)
+			successBlock();
+		
+	} onFailure:^(NSError *error) {
+	
+		if (failureBlock)
+			failureBlock();
+		
+	}];
+
+}
+
 @end
