8aef3d0dc431f8b3955a1e6e926a0273f64871f6
Evadne Wu <ev@monoceroi.com>
2011-12-08 18:18:57 +0800

    initial File Page Element entity implementation

---
+++ wammer/WAFilePageElement+WARemoteInterfaceEntitySyncing.h
@@ -0,0 +1,13 @@
+//
+//  WAFilePageElement+WARemoteInterfaceEntitySyncing.h
+//  wammer
+//
+//  Created by Evadne Wu on 12/8/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import "WAFilePageElement.h"
+
+@interface WAFilePageElement (WARemoteInterfaceEntitySyncing)
+
+@end
