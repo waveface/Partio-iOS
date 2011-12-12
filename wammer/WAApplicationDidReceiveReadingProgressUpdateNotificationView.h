1b4de0a0950b94466e0d4329bf04e59ae78f3675
Evadne Wu <ev@monoceroi.com>
2011-12-12 18:43:23 +0800

    updates IRFoundations

---
+++ wammer/WAApplicationDidReceiveReadingProgressUpdateNotificationView.h
@@ -0,0 +1,13 @@
+//
+//  WAApplicationDidReceiveReadingProgressUpdateNotificationView.h
+//  wammer
+//
+//  Created by Evadne Wu on 12/12/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WAApplicationDidReceiveReadingProgressUpdateNotificationView : UIView
+
+@end
---
+++ wammer/WAApplicationDidReceiveReadingProgressUpdateNotificationView.m
@@ -0,0 +1,31 @@
+//
+//  WAApplicationDidReceiveReadingProgressUpdateNotificationView.m
+//  wammer
+//
+//  Created by Evadne Wu on 12/12/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import "WAApplicationDidReceiveReadingProgressUpdateNotificationView.h"
+
+@implementation WAApplicationDidReceiveReadingProgressUpdateNotificationView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
