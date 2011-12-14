ee009264dba1c9cb11b5378807d36c3278b1d449
Evadne Wu <ev@monoceroi.com>
2011-12-14 17:49:46 +0800

    adds Fetching Conveniences to the data store

---
+++ wammer/WADataStore+FetchingConveniences.h
@@ -0,0 +1,17 @@
+//
+//  WADataStore+FetchingConveniences.h
+//  wammer
+//
+//  Created by Evadne Wu on 12/14/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import "WADataStore.h"
+
+@interface WADataStore (FetchingConveniences)
+
+- (void) fetchLatestArticleInGroup:(NSString *)aGroupIdentifier onSuccess:(void(^)(NSString *identifier, WAArticle *article))callback;
+
+- (void) fetchLatestArticleInGroup:(NSString *)aGroupIdentifier usingContext:(NSManagedObjectContext *)aContext onSuccess:(void(^)(NSString *identifier, WAArticle *article))callback;
+
+@end
---
+++ wammer/WADataStore+FetchingConveniences.m
@@ -0,0 +1,46 @@
+//
+//  WADataStore+FetchingConveniences.m
+//  wammer
+//
+//  Created by Evadne Wu on 12/14/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import "WADataStore+FetchingConveniences.h"
+
+@implementation WADataStore (FetchingConveniences)
+
+- (void) fetchLatestArticleInGroup:(NSString *)aGroupIdentifier onSuccess:(void(^)(NSString *identifier, WAArticle *article))callback {
+
+  [self fetchLatestArticleInGroup:aGroupIdentifier usingContext:[self disposableMOC] onSuccess:callback];
+
+}
+
+- (void) fetchLatestArticleInGroup:(NSString *)aGroupIdentifier usingContext:(NSManagedObjectContext *)context onSuccess:(void(^)(NSString *identifier, WAArticle *article))callback {
+  
+  if (!callback)
+    return;
+
+  NSFetchRequest *fetchRequest = [self.persistentStoreCoordinator.managedObjectModel fetchRequestFromTemplateWithName:@"WAFRArticles" substitutionVariables:[NSDictionary dictionary]];
+  
+  fetchRequest.sortDescriptors = [NSArray arrayWithObjects:
+    [NSSortDescriptor sortDescriptorWithKey:@"timestamp" ascending:NO],
+  nil];
+  
+  fetchRequest.fetchLimit = 1;
+  
+  NSError *fetchingError = nil;
+  NSArray *fetchedArticles = [context executeFetchRequest:fetchRequest error:&fetchingError];
+  
+  if (!fetchedArticles) {
+    NSLog(@"%s: %@", __PRETTY_FUNCTION__, fetchingError);
+    callback(nil, nil);
+    return;
+  }
+  
+  WAArticle *fetchedArticle = [fetchedArticles lastObject];
+  callback(fetchedArticle.identifier, fetchedArticle);
+
+}
+
+@end
