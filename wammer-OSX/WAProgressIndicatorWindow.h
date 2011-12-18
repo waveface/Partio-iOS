7a4218a309b5ba9c46140e48f0e3798d853701f3
Evadne Wu <ev@monoceroi.com>
2011-12-19 04:16:41 +0800

    updates IRKeychain with a corner case

---
+++ wammer-OSX/WAProgressIndicatorWindow.h
@@ -0,0 +1,13 @@
+//
+//  WAProgressIndicatorWindow.h
+//  wammer
+//
+//  Created by Evadne Wu on 12/19/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WAProgressIndicatorWindow : NSWindow
+
+@end
---
+++ wammer-OSX/WAProgressIndicatorWindow.m
@@ -0,0 +1,13 @@
+//
+//  WAProgressIndicatorWindow.m
+//  wammer
+//
+//  Created by Evadne Wu on 12/19/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import "WAProgressIndicatorWindow.h"
+
+@implementation WAProgressIndicatorWindow
+
+@end
