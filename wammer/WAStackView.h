fdb9abda27b8ceb08e8766d17dfb85069ee46f40
Evadne Wu <ev@monoceroi.com>
2011-12-21 20:32:55 +0800

    removes WorkspaceSettings

---
+++ wammer/WAStackView.h
@@ -0,0 +1,13 @@
+//
+//  WAStackView.h
+//  wammer
+//
+//  Created by Evadne Wu on 12/21/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WAStackView : UIView
+
+@end
---
+++ wammer/WAStackView.m
@@ -0,0 +1,31 @@
+//
+//  WAStackView.m
+//  wammer
+//
+//  Created by Evadne Wu on 12/21/11.
+//  Copyright (c) 2011 Waveface. All rights reserved.
+//
+
+#import "WAStackView.h"
+
+@implementation WAStackView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
