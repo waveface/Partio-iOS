423b52e4788a308e891f7efc01fd7679e4cdfa9a
Evadne Wu <ev@monoceroi.com>
2012-01-03 15:43:34 +0800

    adds WAFile syncing keys

--- wammer/WAFile+WARemoteInterfaceEntitySyncing.h
+++ wammer/WAFile+WARemoteInterfaceEntitySyncing.h
@@ -9,6 +9,28 @@
 #import "WAFile.h"
 #import "WARemoteInterfaceEntitySyncing.h"
 
+#ifndef __WAFile_EntitySyncing__
+#define __WAFile_EntitySyncing__
+
+extern NSString * const kWAFileSyncStrategy;
+typedef NSString * const WAFileSyncStrategy;
+
+extern NSString * const kWAFileSyncDefaultStrategy;	//	kWAFileSyncAdaptiveQualityStrategy
+extern NSString * const kWAFileSyncAdaptiveQualityStrategy;
+//	Sends origin or medium depending on -[WARemoteInterface areExpensiveOperationsAllowed]
+//	Since it only sends one thing, it is the best strategy to use when invoked by user interaction
+
+extern NSString * const kWAFileSyncReducedQualityStrategy;
+//	medium only
+//	Useful for cases where speed is key.  Or when the network connection is bad.
+
+extern NSString * const kWAFileSyncFullQualityStrategy;
+//	origin + medium
+//	This is best used with a background worker since it takes up most time and bandwidth.
+//	Not recommended to be used on a carrier-provided connection (might be a capped 3G)
+
+#endif
+
 @interface WAFile (WARemoteInterfaceEntitySyncing) <WARemoteInterfaceEntitySyncing>
 
 @end
