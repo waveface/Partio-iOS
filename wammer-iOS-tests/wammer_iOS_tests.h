98c0d21363adfa4b9da515ac791b1fa2cbb80482
Evadne Wu <ev@monoceroi.com>
2012-01-05 04:38:13 +0800

    fixes stuff

---
+++ wammer-iOS-tests/wammer_iOS_tests.h
@@ -0,0 +1,13 @@
+//
+//  wammer_iOS_tests.h
+//  wammer-iOS_tests
+//
+//  Created by jamie on 1/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface wammer_iOS_tests : SenTestCase
+
+@end
