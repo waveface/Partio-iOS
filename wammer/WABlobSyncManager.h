d2fb23403238e4c2fba7cf0a3b5511244fb68112
Evadne Wu <ev@monoceroi.com>
2012-01-05 03:36:33 +0800

    updates IRFoundations

---
+++ wammer/WABlobSyncManager.h
@@ -0,0 +1,13 @@
+//
+//  WABlobSyncManager.h
+//  wammer
+//
+//  Created by Evadne Wu on 1/4/12.
+//  Copyright (c) 2012 Waveface. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WABlobSyncManager : NSObject
+
+@end
---
+++ wammer/WABlobSyncManager.m
@@ -0,0 +1,13 @@
+//
+//  WABlobSyncManager.m
+//  wammer
+//
+//  Created by Evadne Wu on 1/4/12.
+//  Copyright (c) 2012 Waveface. All rights reserved.
+//
+
+#import "WABlobSyncManager.h"
+
+@implementation WABlobSyncManager
+
+@end
