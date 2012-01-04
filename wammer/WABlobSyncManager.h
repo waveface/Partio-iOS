28c367feb79a6875c06c47cc34af935e6e8a591c
Evadne Wu <ev@monoceroi.com>
2012-01-05 03:44:22 +0800

    initial WABlobSyncManager implementation

--- wammer/WABlobSyncManager.h
+++ wammer/WABlobSyncManager.h
@@ -7,7 +7,38 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "WADataStore.h"
+
+
+#ifndef __WABlobSyncManager__
+#define __WABlobSyncManager__
+
+enum {
+
+	WABlobSyncManagerUnknownState = 0,
+	WABlobSyncManagerInactiveState = 1,
+	WABlobSyncManagerActiveState = 2
+
+}; typedef NSUInteger WABlobSyncManagerState;
+
+#endif
+
 
 @interface WABlobSyncManager : NSObject
+
++ (id) sharedManager;
+
+- (void) beginPostponingBlobSync;
+- (void) endPostponingBlobSync;
+
+- (int) state;
+- (BOOL) isPerformingBlobSync;	//	Convenience on -state
+
+@end
+
+
+@interface WADataStore (BlobSyncingAdditions)
+
+- (void) enumerateFilesWithSyncableBlobsInContext:(NSManagedObjectContext *)context usingBlock:(void(^)(WAFile *aFile, NSUInteger index, BOOL *stop))block;
 
 @end
