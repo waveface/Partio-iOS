965f92c7e38efe34b647d021f2ea1a3d7eaccc32
Evadne Wu <ev@monoceroi.com>
2012-01-09 05:11:08 +0800

    initial WAArticle fixes to emit file activity changes

--- wammer/WAArticle.m
+++ wammer/WAArticle.m
@@ -25,30 +25,67 @@
 @dynamic fileOrder;
 @dynamic draft;
 
-- (void) awakeFromFetch {
++ (BOOL) automaticallyNotifiesObserversForKey:(NSString *)key {
 
-  [super awakeFromFetch];
-  
-  [self irReconcileObjectOrderWithKey:@"files" usingArrayKeyed:@"fileOrder"];
+	if ([super automaticallyNotifiesObserversForKey:key])
+		return YES;
 	
+	if ([key isEqualToString:@"files"])
+		return YES;
+	
+	return NO;
+
+}
+
+- (id) initWithEntity:(NSEntityDescription *)entity insertIntoManagedObjectContext:(NSManagedObjectContext *)context {
+
+	self = [super initWithEntity:entity insertIntoManagedObjectContext:context];
+	if (!self)
+		return nil;
+	
+	[self addObserver:self forKeyPath:@"files" options:NSKeyValueObservingOptionNew context:nil];
+
+	return self;
+
+}
+
+- (void) irAwake {
+
+	[super irAwake];
+	[self irReconcileObjectOrderWithKey:@"files" usingArrayKeyed:@"fileOrder"];
+	
+}
+
+- (void) dealloc {
+
+	[self removeObserver:self forKeyPath:@"files"];
+	
+	[super dealloc];
+
 }
 
 - (NSArray *) fileOrder {
 
-  return [self irBackingOrderArrayKeyed:@"fileOrder"];
+	return [self irBackingOrderArrayKeyed:@"fileOrder"];
 
 }
 
 - (void) didChangeValueForKey:(NSString *)inKey withSetMutation:(NSKeyValueSetMutationKind)inMutationKind usingObjects:(NSSet *)inObjects {
 
-	[super didChangeValueForKey:inKey withSetMutation:inMutationKind usingObjects:inObjects];
-	
 	if ([inKey isEqualToString:@"files"]) {
     
     [self irUpdateObjects:inObjects withRelationshipKey:@"files" usingOrderArray:@"fileOrder" withSetMutation:inMutationKind];
 		return;
     
   }
+
+	[super didChangeValueForKey:inKey withSetMutation:inMutationKind usingObjects:inObjects];
+	
+}
+
+- (void) observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+
+	NSLog(@"%s %@ %@ %@ %@", __PRETTY_FUNCTION__, keyPath, object, change, context);
 
 }
 
