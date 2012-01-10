7107af31452a13faa0ede4ac89d8c91a99cf3f57
Jamie Sa <james.sa.2013@gmail.com>
2012-01-10 11:45:28 +0800

    days left in cycle

---
+++ waveface-tests/waveface_tests.m
@@ -0,0 +1,32 @@
+//
+//  waveface_tests.m
+//  waveface-tests
+//
+//  Created by jamie on 1/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "waveface_tests.h"
+
+@implementation waveface_tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in waveface-tests");
+}
+
+@end
