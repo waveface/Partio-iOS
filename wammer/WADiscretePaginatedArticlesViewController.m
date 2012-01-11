37e27dabe79321db85198751d98d140df813af63
Jamie Sa <james.sa.2013@gmail.com>
2012-01-11 16:14:37 +0800

    enable reflow in advanced feature

--- wammer/WADiscretePaginatedArticlesViewController.m
+++ wammer/WADiscretePaginatedArticlesViewController.m
@@ -1047,35 +1047,34 @@ static NSString * const kWADiscreteArticlesViewLastUsedLayoutGrids = @"kWADiscre
 }
 
 - (NSArray *) debugActionSheetControllerActions {
-
-	//  __block __typeof__(self) nrSelf = self; 
-
-	return [[super debugActionSheetControllerActions] arrayByAddingObjectsFromArray:[NSArray arrayWithObjects:
 	
-    #if 0
-		
-		[IRAction actionWithTitle:@"Reflow" block: ^ {
-		
-			[nrSelf reloadViewContents];
-		
-		}],
-    
-		[IRAction actionWithTitle:@"Label Smoke" block: ^ {
-		
-			UIViewController *testingVC = [[(UIViewController *)[NSClassFromString(@"IRLabelTestingViewController") alloc] init] autorelease];
-			__block UINavigationController *navC = [[[UINavigationController alloc] initWithRootViewController:testingVC] autorelease];
-			testingVC.navigationItem.rightBarButtonItem = [IRBarButtonItem itemWithTitle:@"Close" action:^{
-				[navC dismissModalViewControllerAnimated:YES];
-			}];
-			navC.modalPresentationStyle = UIModalPresentationFormSheet;
-			[[UIApplication sharedApplication].keyWindow.rootViewController presentModalViewController:navC animated:YES];
-		
-		}],
-    
-    #endif
+	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+	if (![defaults boolForKey:kWAAdvancedFeaturesEnabled])
+		return nil;
 	
-	nil]];
-
+	__block __typeof__(self) nrSelf = self; 
+	
+	return [[super debugActionSheetControllerActions] 
+					arrayByAddingObjectsFromArray:[NSArray arrayWithObjects:
+																				 
+																				 [IRAction actionWithTitle:@"Reflow" block: ^ {
+						[nrSelf reloadViewContents];
+						
+					}],
+																				 
+																				 [IRAction actionWithTitle:@"Label Smoke" block: ^ {
+						
+						UIViewController *testingVC = [[(UIViewController *)[NSClassFromString(@"IRLabelTestingViewController") alloc] init] autorelease];
+						__block UINavigationController *navC = [[[UINavigationController alloc] initWithRootViewController:testingVC] autorelease];
+						testingVC.navigationItem.rightBarButtonItem = [IRBarButtonItem itemWithTitle:@"Close" action:^{
+							[navC dismissModalViewControllerAnimated:YES];
+						}];
+						navC.modalPresentationStyle = UIModalPresentationFormSheet;
+						[[UIApplication sharedApplication].keyWindow.rootViewController presentModalViewController:navC animated:YES];
+						
+					}],
+					nil]];
+	
 }
 
 - (void) setLastReadObjectIdentifier:(NSString *)newLastReadObjectIdentifier {
