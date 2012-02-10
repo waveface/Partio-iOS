e27b2f26580cc6a20ec09f90a97dafbc727c7017
Evadne Wu <ev@monoceroi.com>
2012-02-11 05:07:41 +0800

    WAPreview poking hook

--- wammer/WAPreview.m
+++ wammer/WAPreview.m
@@ -39,20 +39,27 @@
 	
 }
 
-//	- (void) configureWithRemoteDictionary:(NSDictionary *)inDictionary {
-//
-//		[super configureWithRemoteDictionary:inDictionary];
-//		
-//		NSDictionary *graphRep = [inDictionary objectForKey:@"og"];
-//		
-//		if (graphRep) {
-//			if (!self.graphElement)
-//				self.graphElement = [WAOpenGraphElement objectInsertingIntoContext:self.managedObjectContext withRemoteDictionary:graphRep];
-//			else
-//				[self.graphElement configureWithRemoteDictionary:graphRep];
-//		}
-//		
+//	[super configureWithRemoteDictionary:inDictionary];
+//	
+//	NSDictionary *graphRep = [inDictionary objectForKey:@"og"];
+//	
+//	if (graphRep) {
+//		if (!self.graphElement)
+//			self.graphElement = [WAOpenGraphElement objectInsertingIntoContext:self.managedObjectContext withRemoteDictionary:graphRep];
+//		else
+//			[self.graphElement configureWithRemoteDictionary:graphRep];
 //	}
+//	
+//	}
+
++ (NSDictionary *) transformedRepresentationForRemoteRepresentation:(NSDictionary *)incomingRepresentation {
+
+	//	incomingRepresentation should be an open graph element
+	//	But we are converting it to a correct thing
+	
+	return [super transformedRepresentationForRemoteRepresentation:incomingRepresentation];
+
+}
 
 + (NSDictionary *) remoteDictionaryConfigurationMapping {
 
