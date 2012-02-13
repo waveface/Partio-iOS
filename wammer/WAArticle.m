e0641f6e5aba99c990ce14cfe3bab52f7f376aed
Evadne Wu <ev@monoceroi.com>
2012-02-13 13:47:13 +0800

    moves WAArticle additions to category; updates Model with Post soul support

--- wammer/WAArticle.m
+++ wammer/WAArticle.m
@@ -1,105 +1,32 @@
 //
 //  WAArticle.m
-//  wammer-iOS
+//  wammer
 //
-//  Created by Evadne Wu on 7/27/11.
-//  Copyright (c) 2011 Iridia Productions. All rights reserved.
+//  Created by Evadne Wu on 2/13/12.
+//  Copyright (c) 2012 Waveface. All rights reserved.
 //
 
 #import "WAArticle.h"
+#import "WAComment.h"
+#import "WAFile.h"
+#import "WAGroup.h"
+#import "WAPreview.h"
 #import "WAUser.h"
-#import "WADataStore.h"
 
 
 @implementation WAArticle
 
 @dynamic creationDeviceName;
+@dynamic draft;
+@dynamic fileOrder;
 @dynamic identifier;
 @dynamic text;
 @dynamic timestamp;
+@dynamic summary;
 @dynamic comments;
 @dynamic files;
-@dynamic previews;
 @dynamic group;
 @dynamic owner;
-@dynamic fileOrder;
-@dynamic draft;
-
-+ (BOOL) automaticallyNotifiesObserversForKey:(NSString *)key {
-
-	if ([super automaticallyNotifiesObserversForKey:key])
-		return YES;
-	
-	if ([key isEqualToString:@"files"])
-		return YES;
-	
-	return NO;
-
-}
-
-+ (NSSet *) keyPathsForValuesAffectingFiles {
-
-	return [NSSet setWithObjects:@"fileOrder", nil];
-
-}
-
-+ (NSSet *) keyPathsForValuesAffectingFileOrder {
-
-	return [NSSet setWithObjects:@"files", nil];
-
-}
-
-- (id) initWithEntity:(NSEntityDescription *)entity insertIntoManagedObjectContext:(NSManagedObjectContext *)context {
-
-	self = [super initWithEntity:entity insertIntoManagedObjectContext:context];
-	if (!self)
-		return nil;
-	
-	[self addObserver:self forKeyPath:@"files" options:NSKeyValueObservingOptionNew context:nil];
-
-	return self;
-
-}
-
-- (void) irAwake {
-
-	[super irAwake];
-	[self irReconcileObjectOrderWithKey:@"files" usingArrayKeyed:@"fileOrder"];
-	
-}
-
-- (void) dealloc {
-
-	[self removeObserver:self forKeyPath:@"files"];
-	
-	[super dealloc];
-
-}
-
-- (NSArray *) fileOrder {
-
-	return [self irBackingOrderArrayKeyed:@"fileOrder"];
-
-}
-
-- (void) didChangeValueForKey:(NSString *)inKey withSetMutation:(NSKeyValueSetMutationKind)inMutationKind usingObjects:(NSSet *)inObjects {
-
-	if ([inKey isEqualToString:@"files"]) {
-    
-    [self irUpdateObjects:inObjects withRelationshipKey:@"files" usingOrderArray:@"fileOrder" withSetMutation:inMutationKind];
-		return;
-    
-  }
-
-	[super didChangeValueForKey:inKey withSetMutation:inMutationKind usingObjects:inObjects];
-	
-}
-
-- (void) observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
-
-	//	NOT YET
-	//	NSLog(@"%s %@ %@ %@ %@", __PRETTY_FUNCTION__, keyPath, object, change, context);
-
-}
+@dynamic previews;
 
 @end
