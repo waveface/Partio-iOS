8bf74ce7bcc9991ac183b9f2962addf084fe2a24
Evadne Wu <ev@monoceroi.com>
2012-02-16 18:13:28 +0800

    exposes several design constants on WADefines and adds more nuanced bar button image generation

--- wammer/WADefines+iOS.h
+++ wammer/WADefines+iOS.h
@@ -9,7 +9,26 @@
 @class WAAppDelegate;
 extern WAAppDelegate * AppDelegate (void);
 
-@class IRBarButtonItem;
+@class IRBarButtonItem, IRBorder, IRShadow;
+
+extern IRBorder *kWADefaultBarButtonBorder;
+extern IRShadow *kWADefaultBarButtonInnerShadow;
+extern IRShadow *kWADefaultBarButtonShadow;
+
+extern UIFont *kWADefaultBarButtonTitleFont;
+extern UIColor *kWADefaultBarButtonTitleColor;
+extern IRShadow *kWADefaultBarButtonTitleShadow;
+
+extern UIColor *kWADefaultBarButtonGradientFromColor;
+extern UIColor *kWADefaultBarButtonGradientToColor;
+extern NSArray *kWADefaultBarButtonGradientColors;
+extern UIColor *kWADefaultBarButtonBackgroundColor;
+
+extern UIColor *kWADefaultBarButtonHighlightedGradientFromColor;
+extern UIColor *kWADefaultBarButtonHighlightedGradientToColor;
+extern NSArray *kWADefaultBarButtonHighlightedGradientColors;
+extern UIColor *kWADefaultBarButtonHighlightedBackgroundColor;
+
 
 extern BOOL WAIsXCallbackURL (NSURL *anURL, NSString **outCommand, NSDictionary **outParams);
 
@@ -19,6 +38,7 @@ extern IRBarButtonItem * WABackBarButtonItem (NSString *labelText, void(^block)(
 extern UIButton * WAButtonForImage (UIImage *anImage);
 extern UIButton * WAToolbarButtonForImage (UIImage *anImage, NSString *aAccessbilityLabel);
 extern UIImage * WABarButtonImageFromImageNamed (NSString *anImageName);
+extern UIImage * WABarButtonImageWithOptions (NSString *anImageName, UIColor *aColor, IRShadow *aShadow);
 
 extern UIView * WAStandardTitleView (void);
 extern UIView * WAStandardTitleLabel (void);
