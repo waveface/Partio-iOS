cdb8ad39165e71cefabb90a5cba3cecea7a77fc7
Jamie Sa <james.sa.2013@gmail.com>
2012-02-20 16:55:36 +0800

    fix photo picker. give default dismiss action to default ActionSheet.

---
+++ wammer/IRActionSheetController+WAAdditions.h
@@ -0,0 +1,13 @@
+//
+//  IRActionSheetController+WAAdditions.h
+//  wammer
+//
+//  Created by jamie on 2/20/12.
+//  Copyright (c) 2012 Waveface. All rights reserved.
+//
+
+#import "IRActionSheetController.h"
+
+@interface IRActionSheetController (WAAdditions)
+
+@end
