8ded62130d658c7a786f6c294ee7f5f9764ebfc8
Evadne Wu <ev@monoceroi.com>
2012-02-24 05:16:58 +0800

    revises the preview badge; introduces Provider Name support and makes heavy use of dependent keys; fixes various issues related to bindings and change tracking

--- wammer/WAPreviewBadge.h
+++ wammer/WAPreviewBadge.h
@@ -33,15 +33,27 @@ enum {
 
 @property (nonatomic, readwrite, retain) UIFont *titleFont;
 @property (nonatomic, readwrite, retain) UIColor *titleColor;
+@property (nonatomic, readwrite, retain) NSString *titlePlaceholder;
+@property (nonatomic, readwrite, retain) UIColor *titlePlaceholderColor;
+
+@property (nonatomic, readwrite, retain) UIFont *providerNameFont;
+@property (nonatomic, readwrite, retain) UIColor *providerNameColor;
+@property (nonatomic, readwrite, retain) NSString *providerNamePlaceholder;
+@property (nonatomic, readwrite, retain) UIColor *providerNamePlaceholderColor;
+
 @property (nonatomic, readwrite, retain) UIFont *textFont;
 @property (nonatomic, readwrite, retain) UIColor *textColor;
+@property (nonatomic, readwrite, retain) NSString *textPlaceholder;
+@property (nonatomic, readwrite, retain) UIColor *textPlaceholderColor;
+
 @property (nonatomic, readwrite, retain) UIView *backgroundView;
 
 @property (nonatomic, readwrite, assign) CGFloat minimumAcceptibleFullFrameAspectRatio;
 
-@property (nonatomic, readonly, retain) UIImage *image;
 @property (nonatomic, readonly, retain) NSString *title;
+@property (nonatomic, readonly, retain) NSString *providerName;
 @property (nonatomic, readonly, retain) NSString *text;
+@property (nonatomic, readonly, retain) UIImage *image;
 @property (nonatomic, readonly, retain) NSURL *link;
 
 @end
