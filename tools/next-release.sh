3d1e682bec2d1e2082118db95e9d3e878ca08f52
Evadne Wu <ev@monoceroi.com>
2012-05-25 23:30:02 +0800

    fixes up the Next Version script so as not to abuse the master branch

--- tools/next-release.sh
+++ tools/next-release.sh
@@ -5,8 +5,8 @@ NextVersion=`expr $CurrentVersion + 1`
 
 echo $NextVersion
 
-git flow release start $NextVersion
+git flow feature start $NextVersion
 agvtool new-version $NextVersion
 git commit -a -m "Punt"
 git commit --amend
-git flow release finish $NextVersion
+git flow feature finish $NextVersion
