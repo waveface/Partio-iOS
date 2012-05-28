4ca5b04248798449b0c1d22ec00690d89e5472b9
jamie sa (jsa) <james.sa.2013@gmail.com>
2012-05-29 00:39:31 +0800

    finally

--- tools/build-it.sh
+++ tools/build-it.sh
@@ -4,16 +4,19 @@ if [ "`git branch --no-color 2> /dev/null | grep '*' | grep -c feature`" != "1" 
     echo 'You must build this from feature branch.'
 fi
 
+#TODO: need to make sure everything is checked in.
+
+agvtool bump
+git commit -am '+1'
+
 # * jsa-feature/HealthyJenkins becomes HealthyJenkins
 # finish and keep the feature. Don't think that's all. Develop, build, FIXES.
-git flow feature finish -Fk `git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* [a-zA-Z0-9\-]*\/\(.*\)/\1/'`
+git flow feature finish -k `git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* [a-zA-Z0-9\-]*\/\(.*\)/\1/'`
 
 git fetch
 git pull origin develop
 
-agvtool bump
-git commit -am '+1' --amend 
-git push origin develop
+git push origin develop 
 
 TAG=`agvtool what-version -terse`
 git tag -m '$TAG' -a $TAG
