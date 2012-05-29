3ce0dffdcc27ded012ef016e0bd8dbf32f40e79b
jamie sa (jsa) <james.sa.2013@gmail.com>
2012-05-29 13:28:24 +0800

    obtain verison info from develop branch

--- tools/build-it.sh
+++ tools/build-it.sh
@@ -1,21 +1,23 @@
 #!/bin/bash --
 
 if [ "`git branch --no-color 2> /dev/null | grep '*' | grep -c feature`" != "1" ]; then
-    echo 'You must build this from feature branch.'
+    echo 'You must build from feature branch.'
 fi
 
 #TODO: need to make sure everything is checked in.
 
+FEATURE=`git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* [a-zA-Z0-9\-]*\/\(.*\)/\1/'`
+git checkout develop
+git fetch
+git pull origin develop
+
 agvtool bump
+git flow feature checkout $FEATURE
 git commit -am '+1'
 
 # * jsa-feature/HealthyJenkins becomes HealthyJenkins
 # finish and keep the feature. Don't think that's all. Develop, build, FIXES.
-git flow feature finish -k `git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* [a-zA-Z0-9\-]*\/\(.*\)/\1/'`
-
-git fetch
-git pull origin develop
-
+git flow feature finish -k $FEATURE
 git push origin develop 
 
 TAG=`agvtool what-version -terse`
