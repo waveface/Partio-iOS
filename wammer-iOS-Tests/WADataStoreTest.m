5827111d3064d5ff861d2092f6c3fb6ceb511c1b
Evadne Wu <ev@monoceroi.com>
2012-05-31 16:22:04 +0800

    removes unused tests in WADataStoreTests and includes it back into the project

--- wammer-iOS-Tests/WADataStoreTest.m
+++ wammer-iOS-Tests/WADataStoreTest.m
@@ -12,22 +12,6 @@
 
 @implementation WADataStoreTest
 
-- (void) assertFileEquivalancy:(WAArticle *)article {
-
-	NSSet *files = article.files;
-	
-	STAssertEquals([files count], [article.fileOrder count], @"Number of files in the backing array must equal the number of files in the unordered to-many relationship.");
-
-	NSSet *fileIDs = [article.files irMap: ^ (NSManagedObject *obj, BOOL *stop) {
-		return [[obj objectID] URIRepresentation];
-	}];
-	
-	NSSet *orderedFileIDs = [NSSet setWithArray:article.fileOrder];
-	
-	STAssertEqualObjects(fileIDs, orderedFileIDs, @"Number of files in the backing array must equal the number of files in the unordered to-many relationship.");
-
-}
-
 - (WAArticle *) disposableArticleWithContext:(NSManagedObjectContext **)outContext {
 
 	WADataStore *ds = [WADataStore defaultStore];
@@ -49,52 +33,6 @@
 
 }
 
-- (void) testOrderedRelationshipMutationFromSet {
-
-	NSManagedObjectContext *context = nil;
-	WAArticle *article = [self disposableArticleWithContext:&context];
-	
-	WAFile *file = [self newFileInContext:context];
-	[article addFilesObject:file];
-	[self assertFileEquivalancy:article];
-	
-	STAssertTrue([article.fileOrder containsObject:[[file objectID] URIRepresentation]], @"After inserting an entity to the unordered to-many relationship,  the object ID should show up in the backing order array");
-	
-	WAFile *secondFile = [self newFileInContext:context];
-	[article addFilesObject:secondFile];
-	[self assertFileEquivalancy:article];
-
-	STAssertTrue([article.fileOrder containsObject:[[file objectID] URIRepresentation]], @"After inserting an entity to the unordered to-many relationship,  the object ID should show up in the backing order array");
-	STAssertTrue([article.fileOrder containsObject:[[secondFile objectID] URIRepresentation]], @"After inserting an entity to the unordered to-many relationship,  the object ID should show up in the backing order array");
-
-}
-
-- (void) testOrderedRelationshipMutationFromArray {
-
-	NSManagedObjectContext *context = nil;
-	WAArticle *article = [self disposableArticleWithContext:&context];
-	
-	WAFile *file = [self newFileInContext:context];
-	[file.managedObjectContext obtainPermanentIDsForObjects:[NSArray arrayWithObject:file] error:nil];
-	NSMutableArray *newOrder = [article.fileOrder mutableCopy];
-	[newOrder addObject:[[file objectID] URIRepresentation]];
-	article.fileOrder = newOrder;
-	[self assertFileEquivalancy:article];
-	
-	STAssertTrue([article.files containsObject:file], @"After inserting an entity to the unordered to-many relationship,  the object ID should show up in the backing order array.");
-	
-	WAFile *secondFile = [self newFileInContext:context];
-	[secondFile.managedObjectContext obtainPermanentIDsForObjects:[NSArray arrayWithObject:secondFile] error:nil];
-	NSMutableArray *nextOrder = [article.fileOrder mutableCopy];
-	[nextOrder addObject:[[secondFile objectID] URIRepresentation]];
-	article.fileOrder = nextOrder;
-	[self assertFileEquivalancy:article];
-	
-	STAssertTrue([article.files containsObject:file], @"After inserting an entity to the unordered to-many relationship,  the object ID should show up in the backing order array.");
-	STAssertTrue([article.files containsObject:secondFile], @"After inserting an entity to the unordered to-many relationship,  the object ID should show up in the backing order array.");
-
-}
-
 - (void) testRepresentingFile {
 
 	NSManagedObjectContext *context = nil;
@@ -102,10 +40,10 @@
 	STAssertNil(article.representingFile, @"Article %@ should not have a representing file when created");
 
 	WAFile *file1 = [self newFileInContext:context];
-	[article addFilesObject:file1];
+	[[article mutableOrderedSetValueForKey:@"files"] addObject:file1];
 	
 	WAFile *file2 = [self newFileInContext:context];
-	[article addFilesObject:file2];
+	[[article mutableOrderedSetValueForKey:@"files"] addObject:file2];
 	
 	STAssertEqualObjects(file1, article.representingFile, @"Article should use the first file in the files array as an implicit represented file");
 	
@@ -114,9 +52,9 @@
 	
 	STAssertTrue([article.files containsObject:file3], @"Associating a representing file should implicitly add it to the article");
 	
-	[article removeFilesObject:file3];
+	[[article mutableOrderedSetValueForKey:@"files"] removeObject:file2];
 	
-	STAssertEqualObjects([article irObjectAtIndex:0 inArrayKeyed:@"fileOrder"], article.representingFile, @"Removing a representing file from the article’s file collection should revert the implicit representing file to the first one in fileOrder");
+	STAssertEqualObjects([article.files objectAtIndex:0], article.representingFile, @"Removing a representing file from the article’s file collection should revert the implicit representing file to the first one in files");
 	
 }
 
