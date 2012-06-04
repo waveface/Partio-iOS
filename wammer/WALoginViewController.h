847de66330c333212fce968bcd144c4723913752
jamie sa (jsa) <james.sa.2013@gmail.com>
2012-06-04 17:06:35 +0800

    init

---
+++ wammer/WALoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  WALoginViewController.h
+//  wammer
+//
+//  Created by jamie on 6/4/12.
+//  Copyright (c) 2012 Waveface. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WALoginViewController : UIViewController
+
+@end
---
+++ wammer/WALoginViewController.m
@@ -0,0 +1,44 @@
+//
+//  WALoginViewController.m
+//  wammer
+//
+//  Created by jamie on 6/4/12.
+//  Copyright (c) 2012 Waveface. All rights reserved.
+//
+
+#import "WALoginViewController.h"
+
+@interface WALoginViewController ()
+
+@end
+
+@implementation WALoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
