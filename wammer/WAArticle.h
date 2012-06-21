890c03e0cc6c06bbcdfb79726586252119f72d31
Evadne Wu <ev@monoceroi.com>
2012-06-21 13:07:32 +0800

    Updates Model with -[WAArticle isDirty] flag - makes several attributes on the Article entity indexed, and not optional - Article holds `dirty` attribute defaulting to NO

--- wammer/WAArticle.h
+++ wammer/WAArticle.h
@@ -2,12 +2,13 @@
 //  WAArticle.h
 //  wammer
 //
-//  Created by Evadne Wu on 5/15/12.
+//  Created by Evadne Wu on 6/21/12.
 //  Copyright (c) 2012 Waveface. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
+
 #import "CoreData+IRAdditions.h"
 
 @class WAComment, WAFile, WAGroup, WAPreview, WAUser;
@@ -23,6 +24,7 @@
 @property (nonatomic, retain) NSDate * modificationDate;
 @property (nonatomic, retain) NSString * summary;
 @property (nonatomic, retain) NSString * text;
+@property (nonatomic, retain) NSNumber * dirty;
 @property (nonatomic, retain) NSSet *comments;
 @property (nonatomic, retain) NSOrderedSet *files;
 @property (nonatomic, retain) WAGroup *group;
@@ -37,19 +39,16 @@
 - (void)removeCommentsObject:(WAComment *)value;
 - (void)addComments:(NSSet *)values;
 - (void)removeComments:(NSSet *)values;
-
-//	DO NOT USE
-//	- (void)insertObject:(WAFile *)value inFilesAtIndex:(NSUInteger)idx;
-//	- (void)removeObjectFromFilesAtIndex:(NSUInteger)idx;
-//	- (void)insertFiles:(NSArray *)value atIndexes:(NSIndexSet *)indexes;
-//	- (void)removeFilesAtIndexes:(NSIndexSet *)indexes;
-//	- (void)replaceObjectInFilesAtIndex:(NSUInteger)idx withObject:(WAFile *)value;
-//	- (void)replaceFilesAtIndexes:(NSIndexSet *)indexes withFiles:(NSArray *)values;
-//	- (void)addFilesObject:(WAFile *)value;
-//	- (void)removeFilesObject:(WAFile *)value;
-//	- (void)addFiles:(NSOrderedSet *)values;
-//	- (void)removeFiles:(NSOrderedSet *)values;
-
+- (void)insertObject:(WAFile *)value inFilesAtIndex:(NSUInteger)idx;
+- (void)removeObjectFromFilesAtIndex:(NSUInteger)idx;
+- (void)insertFiles:(NSArray *)value atIndexes:(NSIndexSet *)indexes;
+- (void)removeFilesAtIndexes:(NSIndexSet *)indexes;
+- (void)replaceObjectInFilesAtIndex:(NSUInteger)idx withObject:(WAFile *)value;
+- (void)replaceFilesAtIndexes:(NSIndexSet *)indexes withFiles:(NSArray *)values;
+- (void)addFilesObject:(WAFile *)value;
+- (void)removeFilesObject:(WAFile *)value;
+- (void)addFiles:(NSOrderedSet *)values;
+- (void)removeFiles:(NSOrderedSet *)values;
 - (void)addPreviewsObject:(WAPreview *)value;
 - (void)removePreviewsObject:(WAPreview *)value;
 - (void)addPreviews:(NSSet *)values;
