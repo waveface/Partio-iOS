60bd1ba50cc2778061b8ee12b5495f0a0f7dbf99
jamie sa (jsa) <james.sa.2013@gmail.com>
2012-07-09 16:03:30 +0800

    init remote interface for Facebook

---
+++ wammer/WARemoteInterface+Facebook.m
@@ -0,0 +1,13 @@
+//
+//  WARemoteInterface+Facebook.m
+//  IRObjectQueue
+//
+//  Created by jamie on 7/5/12.
+//  Copyright (c) 2012 Iridia Productions. All rights reserved.
+//
+
+#import "WARemoteInterface+Facebook.h"
+
+@implementation WARemoteInterface (Facebook)
+
+@end
