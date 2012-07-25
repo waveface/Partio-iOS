82cba3501f4a94f355d343dbb0545509a3910106
jamie sa (jsa) <james.sa.2013@gmail.com>
2012-07-25 09:59:22 +0800

    add one more attachment test condition

--- wammer-iOS-Tests/WAAttachmentsTests.m
+++ wammer-iOS-Tests/WAAttachmentsTests.m
@@ -12,29 +12,31 @@
 
 @implementation WAAttachmentsTests {
 	NSManagedObjectContext *context;
-	NSArray *changedArticleReps;
+}
+
+- (NSArray *)loadDataFile: (NSString *)fileString {
+	NSString *filePath = [[NSBundle bundleForClass:[self class]]
+												pathForResource:fileString
+												ofType:@"txt"];
+	
+	NSData *inputData = [NSData dataWithContentsOfFile:filePath];
+	return [NSJSONSerialization
+					JSONObjectWithData:inputData
+					options:NSJSONReadingMutableContainers
+					error:nil];
 }
 
 - (void)setUp {
 	context = [[WADataStore defaultStore] disposableMOC];
 	
-	NSString *filePath = [[NSBundle bundleForClass:[self class]]
-												pathForResource:@"PostWithIncompleteAttachmentInformation"
-												ofType:@"txt"];
-	
-	NSData *inputData = [NSData dataWithContentsOfFile:filePath];
-	changedArticleReps = [NSJSONSerialization
-												 JSONObjectWithData:inputData
-												 options:NSJSONReadingMutableContainers
-												 error:nil];
 }
 
 - (void)tearDown {
 	context = nil;
-	changedArticleReps = nil;
 }
 
-- (void)testChangedArticleShouldBeParsedCorrectly {
+- (void)testArticleWithIncompletedAttachmentShouldBeDecorated {
+	NSArray *changedArticleReps = [self loadDataFile:@"PostWithIncompleteAttachmentInformation"];
 	STAssertNotNil(changedArticleReps, @"should be a JSON array");
 	NSArray *touchedArticles = [WAArticle
 															insertOrUpdateObjectsUsingContext:context
@@ -42,10 +44,21 @@
 															usingMapping:nil
 															options:IRManagedObjectOptionIndividualOperations];
 	
-	for (WAArticle *article in touchedArticles) {
-		[context refreshObject:article mergeChanges:NO];
-	}
-	
+	WAArticle *article = [touchedArticles objectAtIndex:0];
+	STAssertTrue([article.files count] == 20, @"attachments should be decorated to 20");
+	[context refreshObject:article mergeChanges:NO];
 	[context save:nil];
+}
+
+- (void)testArticleWithFullInformation {
+	NSArray *articleReps = [self loadDataFile:@"PostWithCompleteAttachmentInformation"];
+	STAssertNotNil(articleReps, @"need to be a vaild JSON");
+	NSArray *trasnformedArticle = [WAArticle
+																 insertOrUpdateObjectsUsingContext:context
+																 withRemoteResponse:articleReps
+																 usingMapping:nil
+																 options:IRManagedObjectOptionIndividualOperations];
+	WAArticle *article = [trasnformedArticle objectAtIndex:0];
+	STAssertTrue([article.files count] == 4, @"");
 }
 @end
