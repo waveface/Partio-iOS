92f16b78e6cd5ea996cd4471532eaf813d59d402
jamie sa (jsa) <james.sa.2013@gmail.com>
2012-08-13 12:07:45 +0800

    for Jenkins

---
+++ wammer-iOS-Tests/fopenfix.c
@@ -0,0 +1,19 @@
+//
+//  fopenfix.c
+//  BrowseOverflow
+//
+//  Created by jamie on 8/12/12.
+//
+//  Source: http://stackoverflow.com/questions/8732393/code-coverage-with-xcode-4-2-missing-files/8733416#8733416
+
+#include <stdio.h>
+
+FILE *fopen$UNIX2003( const char *filename, const char *mode )
+{
+  return fopen(filename, mode);
+}
+
+size_t fwrite$UNIX2003( const void *a, size_t b, size_t c, FILE *d )
+{
+  return fwrite(a, b, c, d);
+}
