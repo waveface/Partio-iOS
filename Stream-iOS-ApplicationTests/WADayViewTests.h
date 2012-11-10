431462d8a926a622268a67ef052f872c63cd0e1b
james.sa.2013@gmail.com <651117x>
2012-11-10 10:26:58 +0800

    Init OCHamcrest.

---
+++ Stream-iOS-ApplicationTests/WADayViewTests.h
@@ -0,0 +1,13 @@
+//
+//  WADayViewTests.h
+//  wammer
+//
+//  Created by jamie on 11/10/12.
+//  Copyright (c) 2012 Waveface. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WADayViewTests : SenTestCase
+
+@end
