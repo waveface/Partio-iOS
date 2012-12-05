5d14d4eecf86ec74bf134973b3223725dfc02cb3
syshen <sysheen@gmail.com>
2012-12-05 17:05:05 +0800

    refactor. Add switch views function for other view controller to change among "Photos", "Events", "Documents", and "Web Pages"

--- wammer/WASlidingMenuViewController.h
+++ wammer/WASlidingMenuViewController.h
@@ -10,9 +10,20 @@
 #import "IIViewDeckController.h"
 #import "WAStatusBar.h"
 
+typedef enum WADayViewSupportedStyle: NSUInteger {
+	WAEventsViewStyle,
+	WAPhotosViewStyle,
+} WADayViewSupportedStyle;
+
 @interface WASlidingMenuViewController : UITableViewController <IIViewDeckControllerDelegate>
 
 @property (nonatomic, weak) id delegate;
 @property (nonatomic, readonly, strong) WAStatusBar *statusBar;
+
++ (UIViewController *)viewControllerForViewStyle:(WADayViewSupportedStyle)viewStyle ;
+
+- (void) switchToViewStyle:(WADayViewSupportedStyle)viewStyle;
+- (void) switchToViewStyle:(WADayViewSupportedStyle)viewStyle date:(NSDate*)date;
+- (void) switchToViewStyle:(WADayViewSupportedStyle)viewStyle date:(NSDate*)date animated:(BOOL)animated;
 
 @end
