71450ec70e416d57097b6e8aca6f77662151cb16
jsa <james.sa.2013@gmail.com>
2012-12-21 16:24:48 +0800

    Indent

--- UnitTests/WATests_iOS.m
+++ UnitTests/WATests_iOS.m
@@ -14,12 +14,12 @@
 
 -(NSInteger)daysFromDate:(NSDate *) endDate
 {
-	NSDate *startDate = [NSDate date];
-     NSInteger startDay=[self ordinalityOfUnit:NSDayCalendarUnit
-          inUnit: NSEraCalendarUnit forDate:startDate];
-     NSInteger endDay=[self ordinalityOfUnit:NSDayCalendarUnit
-          inUnit: NSEraCalendarUnit forDate:endDate];
-     return endDay-startDay;
+  NSDate *startDate = [NSDate date];
+  NSInteger startDay=[self ordinalityOfUnit:NSDayCalendarUnit
+			       inUnit: NSEraCalendarUnit forDate:startDate];
+  NSInteger endDay=[self ordinalityOfUnit:NSDayCalendarUnit
+			     inUnit: NSEraCalendarUnit forDate:endDate];
+  return endDay-startDay;
 }
 @end
 
@@ -27,44 +27,44 @@
 
 - (void)setUp
 {
-    [super setUp];
-    
-    // Set-up code here.
+  [super setUp];
+  
+  // Set-up code here.
 }
 
 - (void)tearDown
 {
-    // Tear-down code here.
-    
-    [super tearDown];
+  // Tear-down code here.
+  
+  [super tearDown];
 }
 
 - (void)testDate
 {
-	NSTimeInterval secondsPerDay = 24 * 60 * 60;
-	NSDate *threeDaysLater = [[NSDate alloc]
-							initWithTimeIntervalSinceNow:secondsPerDay*3];
-						
-	NSCalendar *calendar = [NSCalendar currentCalendar];
-	NSInteger days = [calendar daysFromDate:threeDaysLater];
-	
-	STAssertEquals(days, 3, @"3 days");
+  NSTimeInterval secondsPerDay = 24 * 60 * 60;
+  NSDate *threeDaysLater = [[NSDate alloc]
+		        initWithTimeIntervalSinceNow:secondsPerDay*3];
+  
+  NSCalendar *calendar = [NSCalendar currentCalendar];
+  NSInteger days = [calendar daysFromDate:threeDaysLater];
+  
+  STAssertEquals(days, 3, @"3 days");
 }
 
 - (void)testBitOperation
 {
   __block unsigned char tileMap = 0b00000000;
-	__block unsigned char nextTile = 0b00000000;
+  __block unsigned char nextTile = 0b00000000;
   
   BOOL (^usableTile)(unsigned char) = ^ (unsigned char bitMask) {
-
+    
     for (unsigned char i = 0b10000000; i > 0; i >>= 1)
       if ( !(tileMap & i) ){
         nextTile = i;
         break;
-      }  
-		return (BOOL) ( !(tileMap & bitMask) && (bitMask & nextTile) ) ;
-	};
+      }
+    return (BOOL) ( !(tileMap & bitMask) && (bitMask & nextTile) ) ;
+  };
   
   STAssertTrue( usableTile(0b11001100), @"should be true");
   STAssertFalse( usableTile(0b00110011), @"should be false");
@@ -72,55 +72,57 @@
 }
 
 - (void)testPreferredLanguage {
-	
-	NSString *preferredLanguage = @"en";
-	
-	NSArray *preferredLanguages = [NSLocale preferredLanguages];
-	if ([preferredLanguages count] > 0 && [[preferredLanguages objectAtIndex:0] isEqualToString:@"zh-Hant"]) {
-		preferredLanguage = @"zh_tw";
-	}
-	
-	STAssertEquals(preferredLanguage, @"en", @"should rollback to basics");
-	
+  
+  NSString *preferredLanguage = @"en";
+  
+  NSArray *preferredLanguages = [NSLocale preferredLanguages];
+  if ([preferredLanguages count] > 0 && [[preferredLanguages objectAtIndex:0] isEqualToString:@"zh-Hant"]) {
+    preferredLanguage = @"zh_tw";
+  }
+  
+  STAssertEquals(preferredLanguage, @"en", @"should rollback to basics");
+  
 }
 
 - (void)testWeakStrong{
-	__weak WATests_iOS* weakSelf = self;
-	
-	void (^aBlock)(void) = ^(void){
-		__strong WATests_iOS* fakeSelf = weakSelf;
-		
-		if (fakeSelf) {
-			fakeSelf.aProperty = @"set";
-		}
-	};
-	
-	@autoreleasepool {
-		aBlock();
-	}
-	
-	assertThat(self.aProperty, equalTo(@"set"));
-	
-	void (^aBlockWithoutStrong)(void) = ^(void){
-		if (weakSelf) {
-			weakSelf.aProperty = @"weakSet";
-		}
-	};
-	
-	@autoreleasepool {
+  __weak WATests_iOS* weakSelf = self;
+  
+  void (^aBlock)(void) = ^(void){
+    __strong WATests_iOS* fakeSelf = weakSelf;
+    
+    if (fakeSelf) {
+      fakeSelf.aProperty = @"set";
+    }
+  };
+  
+  @autoreleasepool {
+    aBlock();
+  }
+  
+  assertThat(self.aProperty, equalTo(@"set"));
+  
+  void (^aBlockWithoutStrong)(void) = ^(void){
+    if (weakSelf) {
+      weakSelf.aProperty = @"weakSet";
+    }
+  };
+  
+  @autoreleasepool {
     aBlockWithoutStrong();
-	}
-	
-	assertThat(self.aProperty, equalTo(@"weakSet"));
-	
+  }
+  
+  assertThat(self.aProperty, equalTo(@"weakSet"));
+  
 }
 
 - (void)testSameDay {
-	NSDate *now = [NSDate date];
-	NSCalendar *gregorian = [[NSCalendar alloc]initWithCalendarIdentifier:NSGregorianCalendar];
-	NSDateComponents *beginOfTomorrow = [gregorian components:NSDayCalendarUnit|NSYearCalendarUnit|NSMonthCalendarUnit fromDate:now];
-	beginOfTomorrow.day += 1;
-	NSDate *endOfToday = [gregorian dateFromComponents:beginOfTomorrow];
-	assertThat([now dayEnd], equalTo(endOfToday));
+  NSDate *now = [NSDate date];
+  NSCalendar *gregorian = [[NSCalendar alloc]initWithCalendarIdentifier:NSGregorianCalendar];
+  NSDateComponents *beginOfTomorrow = [gregorian components:NSDayCalendarUnit|NSYearCalendarUnit|NSMonthCalendarUnit fromDate:now];
+  beginOfTomorrow.day += 1;
+  NSDate *endOfToday = [gregorian dateFromComponents:beginOfTomorrow];
+  assertThat([now dayEnd], equalTo(endOfToday));
 }
+
+
 @end
