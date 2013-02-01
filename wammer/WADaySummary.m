36808864e820040ec555421c1192efbdbb26a8e1
Kevin Chiu <kevin.chiu@waveface.com>
2013-02-01 18:27:34 +0800

    Auto refresh the downloaded events, photos, documents, webpages on summary page

--- wammer/WADaySummary.m
+++ wammer/WADaySummary.m
@@ -24,7 +24,6 @@
 @interface WADaySummary ()
 
 @property (nonatomic, strong) WAUser *user;
-@property (nonatomic, strong) NSDate *date;
 @property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
 
 @end
@@ -38,11 +37,19 @@
     self.user = user;
     self.date = date;
     self.managedObjectContext = context;
-
-    NSFetchRequest *request = [[WADataStore defaultStore] newFetchRequestForArticlesOnDate:date];
-    self.articles = [self.managedObjectContext executeFetchRequest:request error:nil];
-    
     self.eventPages = [NSMutableArray array];
+    [self configureSummaryAndEventPages];
+  }
+  return self;
+
+}
+
+- (void)configureSummaryAndEventPages {
+
+  NSFetchRequest *articlesFetchRequest = [[WADataStore defaultStore] newFetchRequestForArticlesOnDate:self.date];
+  self.articles = [self.managedObjectContext executeFetchRequest:articlesFetchRequest error:nil];
+  if ([self.eventPages count] == 0) {
+    // initialization
     if ([self.articles count] == 0) {
       WAEventPageView *eventPageView = [WAEventPageView viewWithRepresentingArticle:nil];
       [self.eventPages addObject:eventPageView];
@@ -53,24 +60,57 @@
         [self.eventPages addObject:eventPageView];
       }
     }
-    
-    self.summaryPage = [WASummaryPageView viewFromNib];
+  } else {
+    // update
+    if ([self.articles count] == 0) {
+      // NO OP
+    } else {
 
+      UIView *eventSuperView = [self.eventPages[0] superview];
+
+      // remove empty event page
+      if ([self.eventPages count] == 1 && ![self.eventPages[0] representingArticle]) {
+        [self.eventPages[0] removeFromSuperview];
+        [self.eventPages removeAllObjects];
+      }
+
+      // insert event pages for new articles which are not existing in current event pages
+      for (WAArticle *article in self.articles) {
+        __block BOOL found = NO;
+        __block NSUInteger foundIndex = 0;
+        [self.eventPages enumerateObjectsUsingBlock:^(WAEventPageView *eventPage, NSUInteger idx, BOOL *stop) {
+	if ([article.identifier isEqualToString:eventPage.representingArticle.identifier]) {
+	  // TODO: the article might have more photos to display and the event page should be updated
+	  found = YES;
+	  foundIndex = idx;
+	  *stop = YES;
+	}
+	if ([article.eventStartDate compare:eventPage.representingArticle.eventStartDate] == NSOrderedDescending) {
+	  *stop = YES;
+	}
+        }];
+        if (!found) {
+	WAEventPageView *eventPageView = [WAEventPageView viewWithRepresentingArticle:article];
+	[eventPageView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleEventPagePressed:)]];
+	[eventSuperView addSubview:eventPageView];
+	[self.eventPages insertObject:eventPageView atIndex:foundIndex];
+        }
+      }
+    }
   }
-  return self;
-
-}
-
-- (void)configureSummaryAndEventPages {
-
-  self.summaryPage.user = self.user;
-  self.summaryPage.date = self.date;
+  
+  if (!self.summaryPage) {
+    self.summaryPage = [WASummaryPageView viewFromNib];
+    [self.summaryPage.photosButton addTarget:self action:@selector(handlePhotosButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [self.summaryPage.documentsButton addTarget:self action:@selector(handleDocumentsButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [self.summaryPage.webpagesButton addTarget:self action:@selector(handleWebpagesButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    self.summaryPage.user = self.user;
+    self.summaryPage.date = self.date;
+  }
   self.summaryPage.numberOfEvents = [self.articles count];
   
   NSFetchRequest *photosFetchRequest = [WAPhotoStreamViewController fetchRequestForPhotosOnDate:self.date];
-  NSArray *photos = [self.managedObjectContext executeFetchRequest:photosFetchRequest error:nil];
   self.summaryPage.numberOfPhotos = [self.managedObjectContext countForFetchRequest:photosFetchRequest error:nil];
-  [self.summaryPage.photosButton addTarget:self action:@selector(handlePhotosButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
   
   NSFetchRequest *documentAccessLogsFetchRequest = [WADocumentStreamViewController fetchRequestForFileAccessLogsOnDate:self.date];
   NSArray *documentAccessLogs = [self.managedObjectContext executeFetchRequest:documentAccessLogsFetchRequest error:nil];
@@ -79,7 +119,6 @@
     [documentFilePathSet addObject:accessLog.filePath];
   }
   self.summaryPage.numberOfDocuments = [documentFilePathSet count];
-  [self.summaryPage.documentsButton addTarget:self action:@selector(handleDocumentsButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
   
   NSFetchRequest *webpageAccessLogsFetchRequest = [WAWebStreamViewController fetchRequestForWebpageAccessLogsOnDate:self.date];
   NSArray *webpageAccessLogs = [self.managedObjectContext executeFetchRequest:webpageAccessLogsFetchRequest error:nil];
@@ -88,7 +127,6 @@
     [webpageFileIdentifierSet addObject:accessLog.file.identifier];
   }
   self.summaryPage.numberOfWebpages = [webpageFileIdentifierSet count];
-  [self.summaryPage.webpagesButton addTarget:self action:@selector(handleWebpagesButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
 
 }
 
