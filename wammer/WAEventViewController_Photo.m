ca9aa6e121aa4ba0114322201fad6ab80596ced8
syshen <sysheen@gmail.com>
2013-02-04 17:51:07 +0800

    load better resolution image in the event view

--- wammer/WAEventViewController_Photo.m
+++ wammer/WAEventViewController_Photo.m
@@ -86,7 +86,7 @@
 
 		dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
 		
-			[cell.imageView irBind:@"image" toObject:file keyPath:@"extraSmallThumbnailImage" options:[NSDictionary dictionaryWithObjectsAndKeys: (id)kCFBooleanTrue, kIRBindingsAssignOnMainThreadOption, nil]];
+			[cell.imageView irBind:@"image" toObject:file keyPath:@"smallThumbnailImage" options:[NSDictionary dictionaryWithObjectsAndKeys: (id)kCFBooleanTrue, kIRBindingsAssignOnMainThreadOption, nil]];
 
 		});
 		
