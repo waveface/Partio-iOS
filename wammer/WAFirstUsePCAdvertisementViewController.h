970c9b44b789abcd2a78e01b4a5eb12125422dd7
Kevin Chiu <kevin.chiu@waveface.com>
2013-02-25 16:46:11 +0800

    Refine welcome page and the last two pages of first use

---
+++ wammer/WAFirstUsePCAdvertisementViewController.h
@@ -0,0 +1,15 @@
+//
+//  WAFirstUsePCAdvertisementViewController.h
+//  wammer
+//
+//  Created by kchiu on 13/2/25.
+//  Copyright (c) 2013年 Waveface. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WAFirstUsePCAdvertisementViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *sendLinkButton;
+
+@end
