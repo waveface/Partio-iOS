004cfbe52f00f6dd8aba2da3da0c30a3f9c666d3
jamie <jamie@waveface.com>
2013-02-26 16:01:47 +0800

    Use UICollectionViewCell subclass instead of a hacking cell.

--- wammer/WACollectionOverviewViewController.m
+++ wammer/WACollectionOverviewViewController.m
@@ -10,7 +10,7 @@
 #import "WAFile+LazyImages.h"
 #import "WAGalleryViewController.h"
 #import "WACollection.h"
-
+#import "WACollectionOverviewViewCell.h"
 @interface WACollectionOverviewViewController ()
 
 @end
@@ -30,7 +30,7 @@
 {
   [super viewDidLoad];
   // Do any additional setup after loading the view from its nib.
-  [self.collectionView registerClass:[UICollectionViewCell class]
+  [self.collectionView registerClass:[WACollectionOverviewViewCell class]
           forCellWithReuseIdentifier:@"WACollectionOverviewViewCell"];
 }
 
@@ -49,25 +49,26 @@
   return [_collection.files count];
 }
 
-- (UICollectionViewCell*)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+- (WACollectionOverviewViewCell*)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
   
-  UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"WACollectionOverviewViewCell" forIndexPath:indexPath];
-  
-  UIImageView *imageView;
-  for (UIView *view in [cell subviews]) {
-    if ([view isKindOfClass:[UIImageView class]]) {
-      imageView = (UIImageView*)view;
-    } else {
-      imageView = [[UIImageView alloc] initWithFrame:(CGRect){0,0,75,75}];
-      [cell addSubview:imageView];
-      cell.clipsToBounds = YES;
-    }
-  }
+  WACollectionOverviewViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"WACollectionOverviewViewCell" forIndexPath:indexPath];
   
   WAFile *target = (WAFile*)[_collection.files objectAtIndex:[indexPath row]];
-  imageView.image = target.smallThumbnailImage;
+
+  if (target.smallThumbnailImage) {
+    cell.imageView.image = target.smallThumbnailImage;
+  } else {
+    [target irObserve:@"smallThumbnailImage"
+              options:NSKeyValueObservingOptionInitial|NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld
+              context:nil
+            withBlock:^(NSKeyValueChange kind, id fromValue, id toValue, NSIndexSet *indices, BOOL isPrior) {
+              dispatch_async(dispatch_get_main_queue(), ^{
+                cell.imageView.image = toValue;
+              });
+            }];
+  }
   
-  return (UICollectionViewCell*)cell;
+  return cell;
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
