35e070ca5fcda4729894f671d2cea6c3bea361bb
jamie <jamie@waveface.com>
2013-03-05 11:37:15 +0800

    Trim down unnecessary image view.

--- wammer/WACollectionViewCell.m
+++ wammer/WACollectionViewCell.m
@@ -19,10 +19,10 @@ NSString *const kCollectionViewCellID = @"WACollectionViewCell";
   if (self) {
 	 NSArray *arrayOfView = [[NSBundle mainBundle] loadNibNamed:@"WACollectionViewCell" owner:self options:nil];
 	 self = [arrayOfView objectAtIndex:0];
-	 self.backgroundImageView.layer.cornerRadius = 10.0f;
-	 self.backgroundImageView.layer.backgroundColor = [UIColor whiteColor].CGColor;
-	 self.backgroundImageView.layer.borderColor = [UIColor colorWithWhite:0.9f alpha:1.0f].CGColor;
-	 self.backgroundImageView.layer.borderWidth = 1.0f;
+	 self.layer.cornerRadius = 10.0f;
+	 self.layer.backgroundColor = [UIColor whiteColor].CGColor;
+	 self.layer.borderColor = [UIColor colorWithWhite:0.9f alpha:1.0f].CGColor;
+	 self.layer.borderWidth = 1.0f;
 	 self.coverImage.layer.cornerRadius = 4.0f;
 	 CAGradientLayer *gradientLayer = [[CAGradientLayer alloc] init];
 	 gradientLayer.frame = _title.frame;
