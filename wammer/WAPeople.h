96929d4e4be2e2cb8725ff2e95cf8a92238086b7
syshen <sysheen@gmail.com>
2013-05-02 11:47:32 +0800

    many to many relationship between article and wapeople

--- wammer/WAPeople.h
+++ wammer/WAPeople.h
@@ -2,7 +2,7 @@
 //  WAPeople.h
 //  wammer
 //
-//  Created by Shen Steven on 4/24/13.
+//  Created by Shen Steven on 5/2/13.
 //  Copyright (c) 2013 Waveface. All rights reserved.
 //
 
@@ -10,15 +10,25 @@
 #import <CoreData/CoreData.h>
 #import "CoreData+IRAdditions.h"
 
+
 @class WAArticle;
 
 @interface WAPeople : IRManagedObject
 
 @property (nonatomic, retain) NSString * avatarURL;
 @property (nonatomic, retain) NSString * email;
-@property (nonatomic, retain) NSString * name;
 @property (nonatomic, retain) NSString * identifier;
+@property (nonatomic, retain) NSString * name;
 @property (nonatomic, retain) NSNumber * state;
 @property (nonatomic, retain) WAArticle *article;
+@property (nonatomic, retain) NSSet *sharedArticles;
+@end
+
+@interface WAPeople (CoreDataGeneratedAccessors)
+
+- (void)addSharedArticlesObject:(WAArticle *)value;
+- (void)removeSharedArticlesObject:(WAArticle *)value;
+- (void)addSharedArticles:(NSSet *)values;
+- (void)removeSharedArticles:(NSSet *)values;
 
 @end
